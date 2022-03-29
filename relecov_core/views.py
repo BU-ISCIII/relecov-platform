dcbd8cb13e612be3443a771560c37bc4a7e9b338
luisiJavo <luis.aranda.lillo@gmail.com>
2022-03-29 14:36:32 +0200

    add link to methodology, change links(<a>) color,conflicts with gentenella template

--- relecov_core/views.py
+++ relecov_core/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {}
-    return render(request, 'relecov_core/index3.html', context)
+    return render(request, 'relecov_core/index.html', context)
 
 def dashboard(request):
     context = {}
