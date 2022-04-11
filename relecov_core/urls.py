591a210caa011d68378a7cd86ab6016fa4449a4d
birbi <luis.aranda.lillo@gmail.com>
2022-04-11 22:35:00 +0200

    pull in house

--- relecov_core/urls.py
+++ relecov_core/urls.py
@@ -1,25 +1,13 @@
-"""relecov URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
-from . import views
+from django.conf import settings
+from relecov_core import views
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('variants/', views.variants, name='variants'),
-    path('documentation/', views.documentation, name='documentation'),
-    path('test_read/', views.readTest, name='test_read'),
+    path("", views.index, name="index"),
+    path("variants", views.variants, name="variants"),
+    path("documentation", views.documentation, name="documentation"),
+    path("plotly_ex/", views.plotly_ex, name="plotly_ex"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- relecov_core/views.py
+++ relecov_core/views.py
@@ -1,87 +1,97 @@
-import csv
-import os
-from django.conf import settings
-#from msilib.schema import File
-#from msilib.schema import File
-#
-
-from django.core.files import File
-#from django.core.files.base import ContentFile
-from django.http import HttpResponse
+from distutils.log import debug
+from multiprocessing import Manager, context
+from relecov_core.models import *
 from django.shortcuts import render
+from relecov_core.utils.feed_db import *
+from relecov_core.utils.random_data import generate_random_sequences, generate_weeks
+from relecov_core.utils.parse_files import *
+from relecov_core.utils.manager import Manager_db
 
-from relecov_platform.settings import BASE_DIR
+# IMPORT FROM UTILS
+from relecov_core.utils import *
+
+# plotly dash
+import dash_core_components as dcc
+import dash_html_components as html
+from django_plotly_dash import DjangoDash
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
 
 
-
-# Create your views here.
 def index(request):
     context = {}
-    return render(request, 'relecov_core/index.html', context)
+    return render(request, "relecov_core/index.html", context)
 
-def dashboard(request):
-    context = {}
-    return render(request, 'relecov_core/methodology.html', context)
-    
+
 def variants(request):
     context = {}
-    return render(request, 'relecov_core/variants.html', context)
+    return render(request, "relecov_core/variants.html", context)
+
 
 def documentation(request):
     context = {}
-    return render(request, 'relecov_core/documentation.html', context)
+    return render(request, "relecov_core/documentation.html", context)
 
-def readTest(request):
-    i = 0
-    header_line = ""
-    data_lines_list = []
-    effect_list = []
-    variant_list = []
-    filter_list = []
-    chromosome_list = []
-    sample_list = []
-    caller_list = []
-    lineage_list = []
-    gene_list = []
-    data_array = []
 
-    with open("relecov_core/docs/variants1.csv") as f:
+# plotly dash example
+def plotly_ex(request):
+    #sequences_list = generate_random_sequences()
+    #weeks_list = generate_weeks()
 
-        for linea in f:
-            if i == 0:
-                #Header, field name
-                header_line = linea
-                print("header: " + linea)
-                i += 1
-            else:
-                #rest of lines
-                data_lines_list.append(linea)
+    #manager = Manager_db("Variant",{})
+    #manager.clear_all_tables()
+    #manager.insert_into_table()
 
-        print("*******************DATA******************************************")
-        for data_line in data_lines_list:
-            data_array.append(data_line.split(","))
-
-        for data in data_array:
-            #fields => POS, REF, ALT, DP, REF_DP, ALT_DP, AF
-            variant_list.append(data[2] + "," + data[3] + "," + data[4] + "," + data[6] + "," + data[7] + "," + data[8]
-                                + "," + data[9])
-            effect_list.append(data[12] + "," + data[13] + "," + data[14])
-            chromosome_list.append(data[1])
-            sample_list.append(data[0])
-            filter_list.append(data[5])
-            caller_list.append(data[15])
-            lineage_list.append(data[16])
-            gene_list.append(data[10])
-
-    #for caller in caller_list:
-        
-    #print("variant_list: " + str(variant_list))
-    #print("effect_list: " + str(effect_list))
-    #print("chromosome_list: " + str(chromosome_list))
-    #print("sample_list: " + str(sample_list))
-    #print("filter_list: " + str(filter_list))
-    #print("caller_list: " + str(caller_list))
-    #print("lineage_list: " + lineage_list)
-    #print("gene_list: " + gene_list)
     
-    return render(request, 'relecov_core/documentation.html', {"reader":lineage_list}) 
+    var = parse_csv_into_list_of_dicts("relecov_core/docs/variantLuisTableCSV.csv")
+    #print(type(var))
+    #print(type(var[0]))
+    #print(var[0]["variant_dict"])
+    #print(var[1]["variant_dict"])
+    #print(var[2]["variant_dict"])
+    
+    for i in range(len(var)):
+        #insert_into_variant_table(var[i]["variant_dict"])
+        #print(var[i]["variant_dict"])
+        #print(var[0])
+       
+    
+    
+    
+        """
+        app = DjangoDash("SimpleExample2")  # replaces dash.Dash
+
+        colors = {"background": "#111111", "text": "#7FDBFF"}
+        # assume you have a "long-form" data frame, see https://plotly.com/python/px-arguments/ for more options
+        df = pd.DataFrame(
+            {"Week": weeks_list, "Sequences": sequences_list, "Variant": lineage_list}
+        )
+
+        fig = px.bar(df, x="Week", y="Sequences", color="Variant", barmode="stack")
+
+        fig.update_layout(
+            plot_bgcolor=colors["background"],
+            paper_bgcolor=colors["background"],
+            font_color=colors["text"],
+        )
+
+        app.layout = html.Div(
+            style={"backgroundColor": colors["background"]},
+            children=[
+                html.H1(
+                    children="Hello Dash",
+                    style={"textAlign": "center", "color": colors["text"]},
+                ),
+                html.Div(
+                    children="Dash: A web application framework for your data.",
+                    style={"textAlign": "center", "color": colors["text"]},
+                ),
+                dcc.Graph(
+                    # id="example-graph-2",
+                    figure=fig
+                ),
+            ],
+        )
+        """
+    return render(request, "relecov_core/documentation.html", {})
