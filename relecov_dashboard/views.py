8b858d02b4eefd97ca0ced453c75b91c5d58e016
birbi <luis.aranda.lillo@gmail.com>
2022-04-15 02:09:14 +0200

    slider ok i have to fix number of marks

--- relecov_dashboard/views.py
+++ relecov_dashboard/views.py
@@ -11,15 +11,18 @@ from django_plotly_dash import DjangoDash
 import plotly.express as px
 import pandas as pd
 
+#from dash import Input, Output#Dash, dcc, html, 
+from dash.dependencies import Input, Output
+
 # IMPORT FROM UTILS
 from relecov_core.utils.random_data import *
 from relecov_core.utils.parse_files import *
 
 
 def index(request):
-    df_table = pd.read_csv("relecov_core/docs/cogUK/table_3_2022-04-12.csv")
+    #df_table = pd.read_csv("relecov_core/docs/cogUK/table_3_2022-04-12.csv")
     sequences_list = generate_random_sequences()
-    weeks_list = generate_weeks()
+    #weeks_list = generate_weeks()
     lineage_list = []
     lineage_week_list = []
     variant_data = parse_csv_into_list_of_dicts(
@@ -40,7 +43,9 @@ def index(request):
             "Variant": lineage_list,
         }
     )
-
+    print(len(df["Week"]))
+    print(df["Week"].min())
+    print(df["Week"].max())
     fig = px.bar(df, x="Week", y="Sequences", color="Variant", barmode="stack")
 
     fig.update_layout(
@@ -61,20 +66,18 @@ def index(request):
                 style={"textAlign": "center", "color": colors["text"]},
             ),
             dcc.Graph(
-                # id="example-graph-2",
+                id="graph-with-slider",
                 figure=fig
                 
             ),
             html.Br(),
             dcc.Slider(
-                df["Week"].min(),
-                df["Week"].max(),
-                #marks={str(week): str(week) for week in df['Week'].unique()},
+                min = df["Week"].min(),
+                max = df["Week"].max(),
                 step=None,
-                value=df["Week"].max(),
-                #marks={str(week): str(week) for week in df['Week'].unique()},
-                #marks={i: f'Label {i}' if i == 1 else str(i) for i in range(1,19)},
-                
+                value=df["Week"].min(),
+                marks={str(week): str(week) for week in df['Week'].unique()},
+                id='week-slider'
             ),
             html.Div(
                 style={"background": "white"},
@@ -85,41 +88,68 @@ def index(request):
                     ),
                     html.Div(
                         style={"border":"1px solid"},    
-                        children=generate_table(df_table)
+                        #children=generate_table(df_table)
                     )
                 ]    
+            )
+       ]
     )
-            
-            
-        ]
-        )
+    @app.callback(
+    Output('graph-with-slider', 'figure'),
+    Input('week-slider', 'value'))
     
-    
-    
-    
-    
+    def update_figure(selected_week):
+        sequences_list2 = []
+        lineage_week_list2 =[]
+        lineage_list2 = []
 
+        for variant in variant_data:
+            
+            if(int(variant["lineage_dict"]["week"]) >= int(selected_week)):
+                lineage_list2.append(variant["lineage_dict"]["lineage"])
+                lineage_week_list2.append(variant["lineage_dict"]["week"])
+                
+        sequences_list2 = sequences_list[len(lineage_list)-len(lineage_list2):]
+        
+        df = pd.DataFrame(
+            {
+                "Week": lineage_week_list2,
+                "Sequences": sequences_list2,
+                "Variant": lineage_list2,
+            }
+        )
+             
+        fig = px.bar(df, x="Week", y="Sequences", color="Variant", barmode="stack",
+                        hover_name="Variant")
+
+        fig.update_layout(transition_duration=500)
+        return fig
+        
     return render(request, "relecov_dashboard/index.html")
 
 
 def index2(request):
     df = pd.read_csv("relecov_core/docs/cogUK/table_3_2022-04-12.csv")
-    
-    
-
-
     app = DjangoDash("SimpleExampleTable")
-    
-    app.layout = html.Div(
-        children = generate_table(df)
-    )
 
-    """
     app.layout = html.Div([
-        html.H4(children='Variant Table'),
-        generate_table(df)
+        html.H6("Change the value in the text box to see callbacks in action!"),
+        html.Div([
+            "Input: ",
+            dcc.Input(id='my-input', value='', type='text')
+        ]),
+        html.Br(),
+        html.Div(id='my-output'),
+
     ])
-    """
+
+
+    @app.callback(
+        Output(component_id='my-output', component_property='children'),
+        Input(component_id='my-input', component_property='value')
+    )
+    def update_output_div(input_value):
+        return f'Output: {input_value}'
 
     return render(request, "relecov_dashboard/index2.html")
 
@@ -144,6 +174,41 @@ def index3(request):
 
     return render(request, "relecov_dashboard/index3.html", context)
 
+def index4(request):
+    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+
+    app = DjangoDash("SimpleExample4")
+
+    app.layout = html.Div([
+        dcc.Graph(id='graph-with-slider'),
+        dcc.Slider(
+            df['year'].min(),
+            df['year'].max(),
+            step=None,
+            value=df['year'].min(),
+            #marks={str(year): str(year) for year in df['year'].unique()},
+            #id='year-slider'
+        )
+    ])
+
+    """
+    @app.callback(
+        Output('graph-with-slider', 'figure'),
+        Input('year-slider', 'value'))
+    def update_figure(selected_year):
+        filtered_df = df[df.year == selected_year]
+
+        fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
+                        size="pop", color="continent", hover_name="country",
+                        log_x=True, size_max=55)
+
+        fig.update_layout(transition_duration=500)
+
+        return fig
+    update_figure(2009)
+    """
+    return render(request, "relecov_dashboard/index4.html")
+"""
 def generate_table(dataframe, max_rows=14):
         return html.Table([
         html.Thead(
@@ -156,3 +221,5 @@ def generate_table(dataframe, max_rows=14):
             ]) for i in range(min(len(dataframe), max_rows))
         ])
     ])
+"""
+
