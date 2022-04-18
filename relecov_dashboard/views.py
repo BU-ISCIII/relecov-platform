327968183fe2b656f562b04e1943ceb14e74083e
luissian <chapado.l@gmail.com>
2022-04-18 15:49:21 +0200

    Fixed errors for bootstrap

--- relecov_dashboard/views.py
+++ relecov_dashboard/views.py
@@ -12,10 +12,11 @@ from django_plotly_dash import DjangoDash
 import plotly.express as px
 import pandas as pd
 
-#from dash import Input, Output#Dash, dcc, html, 
+#from dash import Input, Output#Dash, dcc, html,
 from dash.dependencies import Input, Output
 import dash
-import dash_bootstrap_components as dbc
+
+#import dash_bootstrap_components as dbc
 
 # IMPORT FROM UTILS
 from relecov_core.utils.random_data import *
@@ -27,44 +28,45 @@ def index(request):
     variant_data = parse_csv_into_list_of_dicts(
         "relecov_core/docs/variantLuisTableCSV.csv"
     )
-    
-    app = DjangoDash(name= "VariantGraph", external_stylesheets=[dbc.themes.BOOTSTRAP])  # replaces dash.Dash
-    
-    app.layout = get_variant_graph(variant_data) 
-    
+
+    # app = DjangoDash(name= "VariantGraph", external_stylesheets=[dbc.themes.BOOTSTRAP])
+    app = DjangoDash(name= "VariantGraph")   # replaces dash.Dash
+
+    app.layout = get_variant_graph(variant_data)
+
     @app.callback(
     Output('graph-with-slider', 'figure'),
     Input('week-slider', 'value'))
-    
+
     def update_figure(selected_range):
         print('You have selected "{}"'.format(selected_range))
-    
+
         df = set_dataframe_range_slider(variant_data, selected_range)
 
         fig = px.bar(df, x="Week", y="Sequences", color="Variant", barmode="stack",
                         hover_name="Variant")
 
         fig.update_layout(transition_duration=500)
-        return fig   
-        
+        return fig
+
     return render(request, "relecov_dashboard/index.html")
 
 
 def index2(request):
     max_weeks=0
-    selected_range =[1,19] 
+    selected_range =[1,19]
     df_table = pd.read_csv("relecov_core/docs/cogUK/table_3_2022-04-12.csv")
-    
+
     variant_data = parse_csv_into_list_of_dicts(
         "relecov_core/docs/variantLuisTableCSV.csv"
     )
     df = set_dataframe_range_slider(variant_data,selected_range)
-    
+
     for week in df['Week'].unique():
         max_weeks += 1
-    
-    app = DjangoDash("SimpleExampleRangeSlider", external_stylesheets=[dbc.themes.BOOTSTRAP])  # replaces dash.Dash
 
+    # app = DjangoDash("SimpleExampleRangeSlider", external_stylesheets=[dbc.themes.BOOTSTRAP])  # replaces dash.Dash
+    app = DjangoDash("SimpleExampleRangeSlider")  # replaces dash.Dash
     fig = px.bar(df, x="Week", y="Sequences", color="Variant", barmode="stack")
 
     app.layout = html.Div(
@@ -83,7 +85,7 @@ def index2(request):
                     ),
                 ]
             ),
-          
+
             html.Div(
                 children=[
                     html.Div(
@@ -92,7 +94,7 @@ def index2(request):
                                 className = "card",
                                 id="graph-with-slider",
                                 figure=fig
-                                
+
                             )
                         ]
                     )
@@ -102,12 +104,12 @@ def index2(request):
             html.Div(
                children = dcc.RangeSlider(
                     min = df["Week"].min(),
-                    max = max_weeks, 
+                    max = max_weeks,
                     step="1",
                     value=[int(df["Week"].min()),max_weeks],
                     marks={str(week): str(week) for week in df['Week'].unique()},
                     id='week-slider'
-                ), 
+                ),
 
             ),
             html.Div(
@@ -124,12 +126,12 @@ def index2(request):
                                 children="DISCLAIMER: relecov-platform uses curated sequences for determining the counts of a given lineage. Other sources of information may be reporting cases with partial sequence information or other forms of PCR testing.",
                                 className="card-text"
                             )
-                            
+
                         ]
                     )
-                ]    
+                ]
             ),
-            html.Div(           
+            html.Div(
                 children=generate_table(df_table),
             )
        ]
@@ -138,22 +140,23 @@ def index2(request):
     @app.callback(
     Output('graph-with-slider', 'figure'),
     Input('week-slider', 'value'))
-    
+
     def update_figure(selected_range):
         print('You have selected "{}"'.format(selected_range))
-    
+
         df = set_dataframe_range_slider(variant_data, selected_range)
 
         fig = px.bar(df, x="Week", y="Sequences", color="Variant", barmode="stack",
                         hover_name="Variant")
 
         fig.update_layout(transition_duration=500)
-        return fig   
-    
+        return fig
+
     return render(request, "relecov_dashboard/index2.html")
 
 def index3(request):
-    app = DjangoDash( name= 'SimpleExampleBootstrap', external_stylesheets=[dbc.themes.BOOTSTRAP])
+    # app = DjangoDash( name= 'SimpleExampleBootstrap', external_stylesheets=[dbc.themes.BOOTSTRAP])
+    app = DjangoDash( name= 'SimpleExampleBootstrap')
 
     app.layout = dbc.Container(
         dbc.Alert("Hello Bootstrap!", color="success"),
