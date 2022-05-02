b5e58a150791ae6bdbad9ff0736a2897555c2598
luissian <chapado.l@gmail.com>
2022-05-02 22:16:29 +0200

    create first draft for split data to create sample

--- relecov_core/api/utils/request_handling.py
+++ relecov_core/api/utils/request_handling.py
@@ -1,8 +1,21 @@
 from datetime import datetime
+from relecov_core.core_config import HEADING_FOR_SAMPLE_TABLE, HEADING_FOR_AUTHOR_TABLE
 
 
-def split_sample_data():
-    return
+def split_sample_data(data):
+    """Split the json request into dictionnaries with the right fields """
+    split_data = {"sample": {}, "autor": {}}
+    sample_fields = list(HEADING_FOR_SAMPLE_TABLE.values())
+    author_fields = list(HEADING_FOR_AUTHOR_TABLE.values())
+    for item, value in data.items():
+        if item in sample_fields:
+            split_data['sample'][item] = value
+            continue
+        if item in author_fields:
+            split_data['author'][item] = value
+            continue
+    return split_data
+
 
 def include_instances_in_sample():
     return
