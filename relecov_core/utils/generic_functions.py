9b6faefe723c8cbb01a52e43738b99b37b8fe629
luissian <chapado.l@gmail.com>
2022-05-06 22:00:44 +0200

    define store_file functions

--- relecov_core/utils/generic_functions.py
+++ relecov_core/utils/generic_functions.py
@@ -1,24 +1,25 @@
 import time
 from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+
 # from django.conf import settings
-from relecov_core.core_config import SCHEMAS_UPLOAD_FOLDER
+
 import os
 
 
-def store_file(user_file):
+def store_file(user_file, folder):
     '''
     Description:
         The function save the user input file
     Input:
-        request # contains the session information
+        user_file # contains the file
+        folder      subfolder to store the file
     Return:
-        Return True if the user belongs to Wetlab Manager, False if not
+        file_name
     '''
-
     filename, file_extension = os.path.splitext(user_file.name)
     file_name = filename + '_' + str(time.strftime("%Y%m%d-%H%M%S")) + file_extension
     fs = FileSystemStorage()
-    import pdb; pdb.set_trace()
-    filename = fs.save(file_name, user_file)
-    saved_file = os.path.join(settings.MEDIA_ROOT, SCHEMAS_UPLOAD_FOLDER, file_name)
+    saved_file = os.path.join(settings.MEDIA_ROOT, folder, file_name)
+    filename = fs.save(saved_file, user_file)
     return file_name
