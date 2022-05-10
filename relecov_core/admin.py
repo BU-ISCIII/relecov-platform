70ffe3c3f49718f7d66bd4ad260ec603669ce054
luissian <chapado.l@gmail.com>
2022-05-10 21:09:46 +0200

    Created User Profile with the lab name

--- relecov_core/admin.py
+++ relecov_core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 from relecov_core.models import (
     Analysis,
     Authors,
@@ -9,6 +12,7 @@ from relecov_core.models import (
     Gene,
     Lineage,
     Filter,
+    Profile,
     PublicDatabase,
     PublicDatabaseField,
     Sample,
@@ -18,6 +22,26 @@ from relecov_core.models import (
     Schema,
     SchemaProperties,
 )
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "Profile"
+    fk_name = "user"
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInLine,)
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 
 
 class DocumentAdmin(admin.ModelAdmin):
