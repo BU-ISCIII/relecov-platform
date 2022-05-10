b3080541a984da7d77264ca19832ccddb255de67
luissian <chapado.l@gmail.com>
2022-05-10 21:09:46 +0200

    Created User Profile with the lab name

--- relecov_core/models.py
+++ relecov_core/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from relecov_core.core_config import SCHEMAS_UPLOAD_FOLDER
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    laboratory = models.CharField(max_length=60, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
 
 
 class Document(models.Model):
