0b667d204c1f36b19d72552ca7e1343ac3ee0bc5
luissian <chapado.l@gmail.com>
2022-05-23 23:47:39 +0200

    working on displaying the upload file buttom

--- relecov_core/api/views.py
+++ relecov_core/api/views.py
@@ -1,10 +1,13 @@
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser,  FileUploadParser, FormParser
 
 from rest_framework.decorators import (
     authentication_classes,
     permission_classes,
     api_view,
+    action,
+    parser_classes
 )
 from rest_framework import status
 from rest_framework.response import Response
@@ -13,6 +16,22 @@ from django.http import QueryDict
 from .serializers import CreateSampleSerializer
 
 from .utils.request_handling import split_sample_data, prepare_fields_in_sample
+
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+analysis_data = openapi.Parameter(
+    "analysis_name",
+    openapi.IN_FORM,
+    description="Name of the analsys to be performed",
+    type=openapi.TYPE_STRING,
+)
+analysis_file = openapi.Schema(
+    'upload_file',
+    in_=openapi.IN_BODY,
+    type=openapi.TYPE_FILE,
+    )
+
 
 
 @api_view(["GET"])
@@ -51,18 +70,33 @@ def create_sample_data(request):
         sample_serializer.save()
         return Response("Successful upload information", status=status.HTTP_201_CREATED)
 
+y_param = openapi.Parameter('y',"query", openapi.IN_FORM, type=openapi.TYPE_STRING)
 
+
+@parser_classes((FormParser,MultiPartParser, FileUploadParser,))
+@swagger_auto_schema(
+    method="post",
+    # manual_parameters=[analysis_file],
+    request_body=[y_param],
+    responses={
+        200: "Successful upload information",
+        400: "Bad Request",
+        500: "Internal Server Error",
+    },
+)
+# @action(detail=True, methods=['post'], parser_classes=(MultiPartParser, ), name='upload-excel', url_path='upload-excel')
 @api_view(["POST"])
+@action(detail=False, methods=['post'])
 def analysis_data(request):
     if request.method == "POST":
         data = request.data
         import pdb
-
-        if "long_table" == data["analysis"]:
-            print(data)
+        import pdb; pdb.set_trace()
+        # if "long_table" == data["analysis"]:
+        print(data)
         pdb.set_trace()
-        if "upload_file" in request.FILES:
-            a_file = request.FILES["analysis_file"]
-            print(a_file)
+        # if "upload_file" in request.FILES:
+        #     a_file = request.FILES["analysis_file"]
+        #    print(a_file)
 
     return Response(status=status.HTTP_201_CREATED)
