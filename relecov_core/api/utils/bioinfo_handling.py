15cc7042d2603fddf64752ed985d0fc4d11682d1
birbi <luis.aranda.lillo@gmail.com>
2022-06-09 17:36:26 +0200

    created and modified relecov_core/api/utils/bioinfo_handling.py -> save to BioinfoProcessField (except 5 properties, don't match to relecovSchema),FLAKE_OK

--- relecov_core/api/utils/bioinfo_handling.py
+++ relecov_core/api/utils/bioinfo_handling.py
@@ -3,8 +3,8 @@ import re
 from relecov_core.models import (
     BioinfoProcessField,
     Classification,
-    MetadataVisualization,
-    PropertyOptions,
+    # MetadataVisualization,
+    # PropertyOptions,
     Schema,
     SchemaProperties,
 )
@@ -16,11 +16,11 @@ from relecov_core.core_config import (
     # ERROR_INVALID_SCHEMA,
     # ERROR_SCHEMA_ALREADY_LOADED,
     # SCHEMA_SUCCESSFUL_LOAD,
-    ERROR_SCHEMA_ID_NOT_DEFINED,
-    ERROR_SCHEMA_NOT_DEFINED,
-    HEADING_SCHEMA_DISPLAY,
+    # ERROR_SCHEMA_ID_NOT_DEFINED,
+    # ERROR_SCHEMA_NOT_DEFINED,
+    # HEADING_SCHEMA_DISPLAY,
     # MAIN_SCHEMA_STRUCTURE,
-    NO_SELECTED_LABEL_WAS_DONE,
+    # NO_SELECTED_LABEL_WAS_DONE,
 )
 from django.db import models
 
@@ -31,14 +31,14 @@ def fetch_bioinfo_data(data):
     registers.delete()
 
     list_of_properties = []
-    list_of_values = []
+    # list_of_values = []
     list_of_no_exists = []
     # number_of_sample = data.keys()
     data_in_sample = data.values()
 
     for dat in data_in_sample:
         list_of_properties = list(dat.keys())
-        list_of_values = list(dat.values())
+        # list_of_values = list(dat.values())
 
     for property in list_of_properties:
         if SchemaProperties.objects.filter(property__iexact=property).exists():
@@ -134,11 +134,11 @@ def store_bioinfo_fields(schema_obj, s_properties):
 
 def process_bioinfo_file(json_file, user, apps_name):
     """Check json file and store in database"""
-    list_of_samples = []
+    # list_of_samples = []
     list_of_samples_values = []
     list_of_samples_properties = []
     bioinfo_data = load_bioinfo_file(json_file)
-    list_of_samples = bioinfo_data["full_bioinfo"].keys()
+    # list_of_samples = bioinfo_data["full_bioinfo"].keys()
     print(list_of_samples_values)
     for sample in bioinfo_data["full_bioinfo"]:
         list_of_samples_values = bioinfo_data["full_bioinfo"][sample].values()
@@ -160,192 +160,3 @@ def process_bioinfo_file(json_file, user, apps_name):
     """
     return {"SUCCESS": SCHEMA_SUCCESSFUL_LOAD}
     """
-
-
-def set_chromosome(data):
-    chrom_id = 0
-    if Chromosome.objects.filter(chromosome=data["Chrom"]["chromosome"]).last():
-        chrom_id = (
-            Chromosome.objects.filter(chromosome=data["Chrom"]["chromosome"]).last()
-            # .get_chromosome_id()
-        )
-
-        return chrom_id
-
-    else:
-        chrom_serializer = CreateChromosomeSerializer(data=data["Chrom"])
-        if chrom_serializer.is_valid():
-            chrom_serializer.save()
-            print("chrom_serializer saved")
-
-
-def set_gene(data):
-    gene_id = 0
-    if Gene.objects.filter(gene__iexact=data["Gene"]["gene"]).exists():
-        gene_id = (
-            Gene.objects.filter(gene__iexact=data["Gene"]["gene"]).last()
-            # .get_gene_id()
-        )
-        return gene_id
-    else:
-        gene_serializer = CreateGeneSerializer(data=data["Gene"]["gene"])
-        if gene_serializer.is_valid():
-            gene_serializer.save()
-            print("gene_serializer saved")
-
-
-def set_effect(data):
-    effect_id = 0
-    if Effect.objects.filter(effect__iexact=data["Effect"]["effect"]).exists():
-        effect_id = (
-            Effect.objects.filter(effect__iexact=data["Effect"]["effect"]).last()
-            # .get_effect_id()
-        )
-        return effect_id
-    else:
-        effect_serializer = CreateEffectSerializer(data=data["Effect"])
-        if effect_serializer.is_valid():
-            effect_serializer.save()
-            print("effect_serializer saved")
-
-
-def set_variant_in_sample(data):
-    variant_in_sample_id = 0
-    if VariantInSample.objects.filter(
-        dp__iexact=data["VariantInSample"]["dp"]
-    ).exists():
-        variant_in_sample_id = (
-            VariantInSample.objects.filter(
-                dp__iexact=data["VariantInSample"]["dp"]
-            ).last()
-            # .get_variant_in_sample_id()
-        )
-        return variant_in_sample_id
-    else:
-        variant_in_sample_serializer = CreateVariantInSampleSerializer(
-            data=data["VariantInSample"]
-        )
-        if variant_in_sample_serializer.is_valid():
-            variant_in_sample_serializer.save()
-            print("variant_in_sample_serializer saved")
-
-
-def set_filter(data):
-    filter_id = 0
-    if Filter.objects.filter(filter__iexact=data["Filter"]["filter"]).exists():
-        filter_id = (
-            Filter.objects.filter(filter__iexact=data["Filter"]["filter"]).last()
-            # .get_filter_id()
-        )
-        return filter_id
-    else:
-        filter_serializer = CreateFilterSerializer(data=data["Filter"])
-        if filter_serializer.is_valid():
-            filter_serializer.save()
-            print("filter_serializer saved")
-
-
-def set_position(data):
-    position_id = 0
-    if Position.objects.filter(pos__iexact=data["Position"]["pos"]).exists():
-        position_id = (
-            Position.objects.filter(pos__iexact=data["Position"]["pos"]).last()
-            # .get_position_id()
-        )
-        return position_id
-    else:
-        position_serializer = CreatePositionSerializer(data=data["Position"])
-        if position_serializer.is_valid():
-            position_serializer.save()
-            print("position_serializer saved")
-
-
-def set_sample(data):
-    sample_id = 0
-    if Sample.objects.filter(
-        collecting_lab_sample_id=data["Sample"]["collecting_lab_sample_id"]
-    ).exists():
-        sample_id = (
-            Sample.objects.filter(
-                collecting_lab_sample_id=data["Sample"]["collecting_lab_sample_id"]
-            ).last()
-            # .get_sample_id()
-        )
-
-        return sample_id
-    """
-    else:
-        sample_serializer = CreateSampleSerializer(data=data["Sample"])
-        if sample_serializer.is_valid():
-            sample_serializer.save()
-            print("sample_serializer saved")
-    """
-
-
-def set_variant(data, data_ids):
-    if Variant.objects.filter(ref__iexact=data["Variant"]["ref"]).exists():
-        variant_id = (
-            Variant.objects.filter(ref__iexact=data["Variant"]["ref"])
-            .last()
-            .get_variant_id()
-        )
-        return variant_id
-    else:
-        variant = Variant.objects.create_new_variant(data["Variant"]["ref"], data_ids)
-        variant.save()
-
-        """
-        variant_serializer = CreateVariantSerializer(data=data["Variant"])
-        print(variant_serializer)
-        if variant_serializer.is_valid():
-            variant_serializer.save()
-            print("variant_serializer saved")
-        """
-
-
-def set_lineage(data):
-    lineage_id = 0
-    if Lineage.objects.filter(
-        lineage_name__iexact=data["Lineage"]["lineage_name"]
-    ).exists():
-        lineage_id = (
-            Lineage.objects.filter(lineage_name__iexact=data["Lineage"]["lineage_name"])
-            .last()
-            .get_lineage_id()
-        )
-        return lineage_id
-    else:
-        new_lineage = Lineage.objects.create(
-            lineage_name=data["Lineage"]["lineage_name"]
-        )
-        new_lineage.save()
-        """
-        lineage_serializer = CreateLineageSerializer(
-            data=data["Lineage"]["lineage_name"]
-        )
-        if lineage_serializer.is_valid():
-            lineage_serializer.save()
-            print("lineage_serializer saved")
-        """
-
-
-# this function creates a new Sample register for testing
-def create_sample_register():
-    new_sample = Sample.objects.create(
-        state=SampleState.objects.create(
-            state="pre-recorded",
-            display_string="display_string",
-            description="description",
-        ),
-        user=User.objects.create(password="appapk", username="tere"),
-        metadata_file=Document.objects.create(
-            title="title", file_path="", uploadedFile=""
-        ),
-        collecting_lab_sample_id="200002",
-        sequencing_sample_id="1234",
-        biosample_accession_ENA="456123",
-        virus_name="ramiro",
-        gisaid_id="09876",
-        sequencing_date="2022/8/2",
-    )
-    new_sample.save()
