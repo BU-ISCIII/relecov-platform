9f4e91171e998fa6b8832c8d236daea0658a1862
birbi <luis.aranda.lillo@gmail.com>
2022-06-22 19:05:47 +0200

    changed dashboard structure

--- relecov_dashboard/urls.py
+++ relecov_dashboard/urls.py
@@ -5,9 +5,11 @@ from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("variants", views.variants, name="variants"),
+    path("", views.dashboard, name="dashboard"),
+    path("lineagesInTime", views.lineages_in_time, name="lineages_in_time"),
     path("methodology", views.methodology_index, name="methodology_index"),
+    path("needlePlot", views.needle_plot, name="needle_plot"),
+    path("molecular3D", views.molecular_3D, name="molecular_3D"),
     path("hackatonGroup1", views.hackaton_group1, name="hackaton_group1"),
     path("hackatonGroup2", views.hackaton_group2, name="hackaton_group2"),
     path("hackatonGroup3", views.hackaton_group3, name="hackaton_group3"),
