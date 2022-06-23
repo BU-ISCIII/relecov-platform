28fad6babcf43eb423c23b5a37b23fe61832b5d6
birbi <luis.aranda.lillo@gmail.com>
2022-06-23 20:35:17 +0200

    dashboard modified templates, extracted code from views.py to lineages_in_time_graph.py

--- relecov_dashboard/views.py
+++ relecov_dashboard/views.py
@@ -13,7 +13,9 @@ import os
 from django.conf import settings
 from relecov_core.utils.parse_files import parse_csv_into_list_of_dicts
 from relecov_dashboard.utils.graphics.lineages_in_time_graph import (
+    create_lineage_in_time_graph,
     create_test_variant_graph,
+    get_variant_data,
     set_dataframe_range_slider,
 )
 
@@ -40,30 +42,7 @@ def dashboard(request):
 
 
 def lineages_in_time(request):
-    variant_data = parse_csv_into_list_of_dicts(
-        os.path.join(
-            settings.BASE_DIR, "relecov_core", "docs", "variantLuisTableCSV.csv"
-        )
-    )
-    app = DjangoDash(name="TestVariantGraph")
-    app.layout = create_test_variant_graph(variant_data, [1, 19])
-
-    @app.callback(Output("graph-with-slider", "figure"), Input("week-slider", "value"))
-    def update_figure(selected_range):
-        df = set_dataframe_range_slider(variant_data, selected_range)
-
-        fig = px.bar(
-            df,
-            x="Week",
-            y="Sequences",
-            color="Variant",
-            barmode="stack",
-            hover_name="Variant",
-        )
-
-        fig.update_layout(transition_duration=500)
-
-        return fig
+    create_lineage_in_time_graph()
 
     return render(request, "relecov_dashboard/lineages_in_time.html")
 
