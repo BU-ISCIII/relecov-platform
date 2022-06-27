cd79e48336fd784b5f455ac75a38fbaef23514e7
luisiJavo <luis.aranda.lillo@gmail.com>
2022-06-27 17:41:07 +0200

    dashboard-> changing structure

--- relecov_dashboard/urls.py
+++ relecov_dashboard/urls.py
@@ -5,12 +5,13 @@ from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard"),
-    path("lineagesInTime", views.lineages_in_time, name="lineages_in_time"),
-    path("methodology", views.methodology_index, name="methodology_index"),
-    path("needlePlot", views.needle_plot, name="needle_plot"),
-    path("molecular3D", views.molecular_3D, name="molecular_3D"),
+    path("variantDashboard", views.variant_dashboard, name="variant_dashboard"),
+    path("methodologyDashboard", views.methodology_dashboard, name="methodology_dashboard"),
     path("django_plotly_dash/", include("django_plotly_dash.urls")),
+    # path("needlePlot", views.needle_plot, name="needle_plot"),
+    # path("molecular3D", views.molecular_3D, name="molecular_3D"),
+    # path("", views.dashboard, name="dashboard"),
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
