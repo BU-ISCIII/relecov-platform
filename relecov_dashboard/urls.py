aa95acf0cc599f501d33debf44cc6393933a6b66
luisiJavo <luis.aranda.lillo@gmail.com>
2022-06-28 17:05:01 +0200

    dashboard-> modified structure and files

--- relecov_dashboard/urls.py
+++ relecov_dashboard/urls.py
@@ -1,17 +1,20 @@
 from django.urls import path, include
 from django.conf import settings
 
-from . import views
+from relecov_dashboard import views
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("variantDashboard", views.variant_dashboard, name="variant_dashboard"),
-    path("methodologyDashboard", views.methodology_dashboard, name="methodology_dashboard"),
+    path(
+        "methodologyDashboard",
+        views.methodology_dashboard,
+        name="methodology_dashboard",
+    ),
     path("django_plotly_dash/", include("django_plotly_dash.urls")),
+    path("lineagesVOC", views.lineages_voc, name="lineages_voc"),
     # path("needlePlot", views.needle_plot, name="needle_plot"),
-    # path("molecular3D", views.molecular_3D, name="molecular_3D"),
-    # path("", views.dashboard, name="dashboard"),
-    
+    # path("", views.main_dashboard, name="main_dashboard"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- relecov_dashboard/views.py
+++ relecov_dashboard/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from relecov_dashboard.utils.graphics.lineages_in_time_graph import (
     create_lineage_in_time_graph,
 )
-from relecov_dashboard.utils.graphics.molecule3D_graph import create_molecule3D_graph
+from relecov_dashboard.utils.graphics.molecule3D_graph import (
+    create_molecule3D_graph,
+    create_molecule3D_zoom_specific_residues,
+)
 from relecov_dashboard.utils.graphics.needle_plot_graph import create_needle_plot_graph
 
 
@@ -15,8 +18,17 @@ def variant_dashboard(request):
 def methodology_dashboard(request):
     return render(request, "relecov_dashboard/methodology_dashboard.html")
 
+
+def lineages_voc(request):
+    create_lineage_in_time_graph()
+    create_needle_plot_graph(sample=None)
+    create_molecule3D_graph()
+    create_molecule3D_zoom_specific_residues()
+    return render(request, "relecov_dashboard/dashboard_templates/lineages_voc.html")
+
+
 """
-def dashboard(request):
+def main_dashboard(request):
     return render(request, "relecov_dashboard/dashboard.html")
 
 
@@ -25,7 +37,7 @@ def needle_plot(request):
     return render(request, "relecov_dashboard/needle_plot.html")
 
 
-def molecular_3D(request):
-    # create_molecule3D_graph()
+def molecular_3D_bootstrap(request):
+    create_molecule3D_graph()
     return render(request, "relecov_dashboard/molecular_3D.html")
 """
