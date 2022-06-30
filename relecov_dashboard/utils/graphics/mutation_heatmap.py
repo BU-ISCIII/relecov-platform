a1cd747688edb942c826323b31424e4610d770e6
Pablo Aja Macaya <pablo.aja@udc.es>
2022-06-30 16:23:00 +0200

    Mutation heatmap improvements

--- relecov_dashboard/utils/graphics/mutation_heatmap.py
+++ relecov_dashboard/utils/graphics/mutation_heatmap.py
@@ -1,3 +1,16 @@
+"""
+Mutation heatmap
+
+- Read mutation data
+- Process data
+- Create plotly heatmap:
+    - Rows are samples
+    - Mutations are columns
+    - Color represents allele frequency
+
+"""
+
+
 # Dash libs
 import dash
 from dash.dependencies import Input, Output
@@ -7,60 +20,20 @@ import plotly.express as px
 
 # Other libs
 import pandas as pd
-import json
+
+# Custom functions
+from mutation_table import (  # TODO: Fix this bad import
+    read_mutation_data,
+    process_mutation_df,
+)
 
 
-def read_mutation_data(input_file: str, file_extension: str = "csv") -> pd.DataFrame:
-    """
-    Read mutation data, either in CSV or JSON format.
-    If in JSON format, the JSON must follow a structure of [{'pk': {'atr1':'z'} }]
-    Returns a pandas dataframe object
-    """
-    df = None
-    if file_extension == "json":
-        with open(input_file) as f:
-            # JSON must have a "primary key", which is the sample ID
-            df = pd.DataFrame.from_dict(json.load(f), orient="index")
-            df = df.reset_index().rename(columns={"index": "SAMPLE"})
-    elif file_extension == "csv":
-        df = pd.read_csv(input_file, sep=",")
-    else:
-        raise Exception("Unrecognized file format!")
-
-    return df
-
-
-def process_mutation_df(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Process pd.DataFrame object, selecting specific columns and renaming then as required
-    (maybe usefull for translations)
-    """
-    translate_dicc = {
-        "SAMPLE": "SAMPLE",
-        "POS": "POS",
-        "HGVS_P": "MUTATION",
-        "AF": "AF",
-        "EFFECT": "EFFECT",
-        "GENE": "GENE",
-        "LINEAGE": "LINEAGE",
-    }
-    # Rename and select columns
-    df = df[translate_dicc.keys()]  # select specific keys
-    df = df.rename(columns=translate_dicc)  # rename to the required output names
-
-    return df
-
-
-def create_mutation_heatmap(input_file: str, sample_ids: list) -> dash.Dash:
+def create_mutation_heatmap(df: pd.DataFrame, sample_ids: list) -> dash.Dash:
     """
     Create mutation heatmap, where each row is a sample and each column a mutation
     The color is based on the allele frequency of the mutation
     """
     # ---- Set up ----
-    # Read data
-    df = read_mutation_data(input_file, file_extension="csv")
-    df = process_mutation_df(df)
-
     # Read some extra values
     all_genes = list(df["GENE"].unique())
     all_sample_ids = list(df["SAMPLE"].unique())
@@ -112,7 +85,6 @@ def create_mutation_heatmap(input_file: str, sample_ids: list) -> dash.Dash:
 
     app.layout = html.Div(
         children=[
-            html.P(id="mutation_heatmap-message"),
             html.Div(
                 style={
                     "display": "flex",
@@ -126,20 +98,19 @@ def create_mutation_heatmap(input_file: str, sample_ids: list) -> dash.Dash:
                         clearable=False,
                         multi=True,
                         value=sample_ids,
-                        style={"width": "400px", "margin-right": "30px"},
+                        style={"width": "500px", "margin-right": "30px"},
                     ),
                     dcc.Dropdown(
                         id="mutation_heatmap-gene_dropdown",
                         options=[{"label": i, "value": i} for i in all_genes],
                         clearable=False,
                         multi=True,
-                        value=all_genes,
+                        value=None,
                         style={"width": "400px", "margin-right": "30px"},
                         placeholder="Filter genes",
                     ),
                 ],
             ),
-            html.Br(),
             dcc.Graph(
                 id="mutation_heatmap",
                 figure=get_figure(df, sample_ids),
@@ -179,6 +150,10 @@ if __name__ == "__main__":
     )
     sample_ids = [214821, 220685, 214826, 214825]
 
+    # Read data
+    df = read_mutation_data(input_file, file_extension="csv")
+    df = process_mutation_df(df)
+
     # App
-    app = create_mutation_heatmap(input_file, sample_ids)
+    app = create_mutation_heatmap(df, sample_ids)
     app.run_server(debug=True)
