500288ad7d8748bbb0c3728a8bd2665801908263
alejandra-gonzalezs <alejandrags2305@gmail.com>
2022-07-01 16:09:49 +0200

    fixes in viewing residue in color, addded comments for pycharm/web interchange

--- relecov_dashboard/utils/graphics/mutations_3D_molecule.py
+++ relecov_dashboard/utils/graphics/mutations_3D_molecule.py
@@ -1,10 +1,11 @@
 import os
 from django_plotly_dash import DjangoDash
+#import dash
 import dash_table
 from dash.dependencies import Input, Output
 import pandas as pd
 import dash_bio as dashbio
-import dash_html_components as html
+from dash import html
 from dash_bio.utils import create_mol3d_style
 from relecov_platform import settings
 
@@ -82,19 +83,22 @@ class PdbParser:
 
         return data
 
+def create_mol3d_style(atoms):
+    atom_styles = []
+    for a in atoms:
+        atom_styles.append({"visualization_type": "cartoon", "color": "#ced4da"})
+    return atom_styles
 
 def get_spike_mutations(csv_file):
     df = pd.read_csv(csv_file, sep=",")
     spike_df = df.loc[df["GENE"] == "S"]
     return spike_df
-alejandra-gonzalezs/relecov-platform
 
 def get_table_selection(df):
     table_selection = df[["POS", "REF", "ALT", "HGVS_C", "HGVS_P", "HGVS_P_1LETTER"]]
     table_selection = table_selection.drop_duplicates()
     table_selection = table_selection.sort_values(by=["POS"])
     return table_selection
-
 
 def create_graph():
     """
@@ -113,6 +117,7 @@ def create_graph():
         )
     )
     """
+
     file_csv = os.path.join(
         settings.BASE_DIR,
         "relecov_dashboard",
@@ -120,22 +125,25 @@ def create_graph():
         "csv_files",
         "variants_long_table_last.csv",
     )
-    # file_csv = "/home/vhir/Documents/biohackathon_relecov/Hackaton/variants_long_table.csv"
+
+    #file_csv = "/home/vhir/Documents/biohackathon_relecov/Hackaton/variants_long_table.csv"
 
     spike_mutations = get_table_selection(get_spike_mutations(file_csv))
 
     app = DjangoDash("model3D")
+
     pdb_file = PdbParser(
         os.path.join(
             settings.BASE_DIR, "relecov_dashboard", "utils", "pdb_files", "7dwz.pdb"
         )
     )
 
-    # parser = PdbParser("/home/vhir/Documents/biohackathon_relecov/Hackaton/7dwz.pdb")
+
+    #pdb_file = PdbParser("/home/vhir/Documents/biohackathon_relecov/Hackaton/7dwz.pdb")
     # structure = pmd.load_file("/home/vhir/Documents/biohackathon_relecov/Hackaton/7dwz.pdb")
     # import pdb; pdb.set_trace()
     data = pdb_file.mol3d_data()
-    styles = create_mol3d_style(data["atoms"], visualization_type="cartoon")
+    styles = create_mol3d_style(data["atoms"])
 
     df = pd.DataFrame(data["atoms"])
 
@@ -144,17 +152,21 @@ def create_graph():
     app.layout = html.Div(
         [
             dash_table.DataTable(
-                id="zooming-specific-residue-table",
-                columns=[{"name": i, "id": i} for i in spike_mutations.columns],
-                data=spike_mutations.to_dict("records"),
+                id="selecting-specific-spike-residue-table",
+                columns=[{"name": i, "id": i} for i in df.columns],
+                data=df.to_dict("records"),
                 row_selectable="single",
                 page_size=10,
             ),
             dashbio.Molecule3dViewer(
-                id="zooming-specific-molecule3d-zoomto", modelData=data, styles=styles
+                id="zooming-specific-molecule3d-zoomto",
+                modelData=data,
+                styles=styles,
+                selectionType="residue",
             ),
         ]
-        )
+    )
+
     @app.callback(
         Output("zooming-specific-molecule3d-zoomto", "zoomTo"),
         Output("zooming-specific-molecule3d-zoomto", "labels"),
@@ -171,7 +183,7 @@ def create_graph():
         atoms = df[df["residue_position"] == row["residue_position"].iloc[0]]
         list_atoms = atoms["serial"].tolist()
         new_atom_styles = []
-        for a in range(len(styles_data)):
+        for a in range(len(styles)):
             if a in list_atoms:
                 new_atom_styles.append(
                     {"visualization_type": "cartoon", "color": "#ff7d00"}
@@ -199,3 +211,9 @@ def create_graph():
             ],
             new_atom_styles,
         ]
+
+    #app.runserver()
+
+#if __name__ == "__main__":
+#    create_graph()
+
