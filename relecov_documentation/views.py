294855490a58a3c1abf3e5722f86e3f190da765e
luisiJavo <luis.aranda.lillo@gmail.com>
2022-07-20 15:32:37 +0200

    relecov_documentation-> testing md, images loads

--- relecov_documentation/views.py
+++ relecov_documentation/views.py
@@ -6,6 +6,8 @@ from relecov_documentation.utils.markdown_handling import (
     generate_html_from_markdown_file,
 )
 
+from django.http import Http404
+
 
 # Create your views here.
 def documentation(request):
@@ -22,7 +24,10 @@ def documentation(request):
 
 def documentation_login(request):
     html_visualization_from_markdown = generate_html_from_markdown_file("login.md")
+    # if html_visualization_from_markdown == "ERROR FILE NOT FOUND":
+    #     raise Http404("File does not exist")
 
+    
     return render(
         request,
         "relecov_documentation/documentation.html",
