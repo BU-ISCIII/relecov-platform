7d9092402c85e3e9268d81f59a2b1b95c802169b
birbi <luis.aranda.lillo@gmail.com>
2022-07-21 19:28:10 +0200

    relecov_documentation->markdown_handling.py->split md file into title,subtitle and body, modified md files

--- relecov_documentation/utils/markdown_handling.py
+++ relecov_documentation/utils/markdown_handling.py
@@ -19,3 +19,29 @@ def generate_html_from_markdown_file(markdown_file):
         return "ERROR FILE NOT FOUND"
 
     return html
+
+
+def generate_html_from_markdown_file2(markdown_file):
+    html_dict = {}
+    html_list = []
+    html = ""
+    try:
+        markdown_doc = os.path.join(
+            settings.BASE_DIR, "relecov_documentation", "markdown_files", markdown_file
+        )
+        with open(markdown_doc, "r") as fh:
+            for line in fh:
+                html_list.append(line)
+                # html += markdownify(line)
+
+    except FileNotFoundError:
+        print("File doesn't exists")
+        return "ERROR FILE NOT FOUND"
+    for html_line in html_list[4:]:
+        html += markdownify(html_line)
+
+    html_dict["title"] = markdownify(html_list[0])
+    html_dict["sub_title"] = markdownify(html_list[2])
+    html_dict["body"] = html
+
+    return html_dict
