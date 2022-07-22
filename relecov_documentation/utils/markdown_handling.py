2e0e5dc911980175cfb0bb3d71be4e1713896fb9
luisiJavo <luis.aranda.lillo@gmail.com>
2022-07-22 11:32:26 +0200

    relecov_documentation -> clean some code

--- relecov_documentation/utils/markdown_handling.py
+++ relecov_documentation/utils/markdown_handling.py
@@ -6,22 +6,6 @@ from markdownx.utils import markdownify
 
 
 def generate_html_from_markdown_file(markdown_file):
-    html = ""
-    try:
-        markdown_doc = os.path.join(
-            settings.BASE_DIR, "relecov_documentation", "markdown_files", markdown_file
-        )
-        with open(markdown_doc, "r") as fh:
-            for line in fh:
-                html += markdownify(line)
-    except FileNotFoundError:
-        print("File doesn't exists")
-        return "ERROR FILE NOT FOUND"
-
-    return html
-
-
-def generate_html_from_markdown_file2(markdown_file):
     html_dict = {}
     html_list = []
     html = ""
--- relecov_documentation/views.py
+++ relecov_documentation/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render  # , redirect
 # from django.contrib.auth.decorators import login_required
 
 from relecov_documentation.utils.markdown_handling import (
-    generate_html_from_markdown_file2,
+    generate_html_from_markdown_file,
 )
 
 
 # Create your views here.
 def documentation(request):
-    html_visualization_from_markdown = generate_html_from_markdown_file2(
+    html_visualization_from_markdown = generate_html_from_markdown_file(
         "documentation.md"
     )
 
@@ -21,7 +21,7 @@ def documentation(request):
 
 
 def documentation_create_user_account(request):
-    html_visualization_from_markdown = generate_html_from_markdown_file2(
+    html_visualization_from_markdown = generate_html_from_markdown_file(
         "create_user_account.md"
     )
 
@@ -33,7 +33,7 @@ def documentation_create_user_account(request):
 
 
 def documentation_access_to_intranet(request):
-    html_visualization_from_markdown = generate_html_from_markdown_file2(
+    html_visualization_from_markdown = generate_html_from_markdown_file(
         "access_to_intranet.md"
     )
 
