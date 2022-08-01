408f7abf74a2ea1968e98c83b761022bb8851b72
luissian <chapado.l@gmail.com>
2022-08-02 00:04:22 +0200

    test12 flake8

--- relecov_core/views.py
+++ relecov_core/views.py
@@ -74,6 +74,7 @@ def bio_info_json_handling(request):
         request, "relecov_core/bioInfoJSONHandling.html", {"schemas": schemas}
     )
     """
+    # test flake
     return render(request, "relecov_core/bioInfoJSONHandling.html", {})
 
 
