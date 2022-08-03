50ae6281646e25adbc9c7bab6a0c2d3f3ede3e63
luisiJavo <luis.aranda.lillo@gmail.com>
2022-08-03 13:22:17 +0200

    relecov_documentation->created more testing markdown files,views and urls, lintin OK2

--- relecov_documentation/urls.py
+++ relecov_documentation/urls.py
@@ -23,8 +23,16 @@ urlpatterns = [
     path("markdownx/", include("markdownx.urls")),
     path("UploadMetadataLab/", views.upload_metadata_lab, name="upload_metadata_lab"),
     path("ResultsDownload/", views.results_download, name="results_download"),
-    path("ResultsInfoProcessed/", views.results_info_processed, name="results_info_processed"),
-    path("ResultsInfoReceived/", views.results_info_received, name="results_info_received"),
+    path(
+        "ResultsInfoProcessed/",
+        views.results_info_processed,
+        name="results_info_processed",
+    ),
+    path(
+        "ResultsInfoReceived/",
+        views.results_info_received,
+        name="results_info_received",
+    ),
     path("UploadToEna/", views.upload_to_ena, name="upload_to_ena"),
     path("UploadToGisaid/", views.upload_to_gisaid, name="upload_to_gisaid"),
     path("Test/", views.test, name="test"),
