2f0652f01d31744da069266464ad1f51b41597e4
luissian <chapado.l@gmail.com>
2022-08-24 00:32:57 +0200

    remove batchSample class

--- relecov_core/admin.py
+++ relecov_core/admin.py
@@ -33,8 +33,7 @@ from relecov_core.models import (
     TemporalSampleStorage,
     Variant,
     VariantInSample,
-    DateUpdateState,
-    BatchSample,
+    DateUpdateState
 )
 
 
@@ -221,7 +220,6 @@ admin.site.register(Schema, SchemaAdmin)
 admin.site.register(SchemaProperties, SchemaPropertiesAdmin)
 admin.site.register(PropertyOptions, PropertyOptionsAdmin)
 admin.site.register(MetadataVisualization, MetadataVisualizationAdmin)
-admin.site.register(BatchSample)
 admin.site.register(BioinfoAnalysisField, BioinfoAnalysisFielddAdmin)
 admin.site.register(BioInfoAnalysisValue, BioInfoAnalysisValueAdmin)
 admin.site.register(Classification, ClassificationAdmin)
