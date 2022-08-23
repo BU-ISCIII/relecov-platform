96a81199ca0255d7ade8f5a78387f7454734f255
luissian <chapado.l@gmail.com>
2022-08-24 00:36:11 +0200

    fixing bug lineage fields were not store in database

--- relecov_core/api/utils/common_functions.py
+++ relecov_core/api/utils/common_functions.py
@@ -1,4 +1,4 @@
-from relecov_core.models import Schema
+from relecov_core.models import Schema, Sample
 
 
 def get_schema_version_if_exists(data):
@@ -16,4 +16,13 @@ def get_schema_version_if_exists(data):
                 schema_version__iexact=data["schema_version"],
                 schema_apps_name__iexact=apps_name,
             ).last()
+    return None
+
+
+def get_sample_obj_if_exists(data):
+    """Check if sample name exists"""
+    if Sample.objects.filter(sequencing_sample_id__iexact=data["sample_name"]).exists():
+        return Sample.objects.filter(
+            sequencing_sample_id__iexact=data["sample_name"]
+        ).last()
     return None
