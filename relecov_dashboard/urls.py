2540435141aef7da2c3c619d1e0aad2ea345f888
luissian <chapado.l@gmail.com>
2022-08-29 00:06:27 +0200

    changed the methodology index page

--- relecov_dashboard/urls.py
+++ relecov_dashboard/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
     path("variants", views.variants_index, name="variants_index"),
     path("django_plotly_dash/", include("django_plotly_dash.urls")),
     path(
+        "methodology/fields_utilization",
+        views.methodology_fields_utilization,
+        name="methodology_fields_utilization",
+    ),
+    path(
         "variants/lineagesVoc",
         views.variants_lineages_voc,
         name="variants_lineages_voc",
@@ -36,12 +41,7 @@ urlpatterns = [
     path("spikeMutations", views.spike_mutations, name="spike_mutations"),
     path("Gauge", views.gauge_test, name="gauge"),
     # Methodology graphics
-    path("methodology", views.methodology_index, name="methodology_index"),
-    path(
-        "methodology/fields_utilization",
-        views.methodology_fields_utilization,
-        name="methodology_fields_utilization",
-    ),
+    path("methodology", views.methodology_index, name="methodology_index")
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
