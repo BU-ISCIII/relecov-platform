48bfd3928db5124baeca50bacaa873c1bce392ef
luisiJavo <luis.aranda.lillo@gmail.com>
2022-08-31 13:45:42 +0200

    dashboard-> mutation table->loads 1 sample OK

--- relecov_core/utils/handling_samples.py
+++ relecov_core/utils/handling_samples.py
@@ -278,8 +278,8 @@ def get_sample_display_data(sample_id, user):
 
 def get_sample_obj_from_sample_name(sample_name):
     """Return the sample instance from its name"""
-    if Sample.objects.filter(sample_name__iexact=sample_name).exists():
-        return Sample.objects.filter(sample_name__iexact=sample_name).last()
+    if Sample.objects.filter(sequencing_sample_id__iexact=sample_name).exists():
+        return Sample.objects.filter(sequencing_sample_id__iexact=sample_name).last()
     return None
 
 
