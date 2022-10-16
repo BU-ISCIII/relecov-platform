322ee489b61804ebc3d293631fc0288f6602634f
luissian <chapado.l@gmail.com>
2022-10-16 17:50:10 +0200

    clean up and organize files

--- relecov_documentation/urls.py
+++ relecov_documentation/urls.py
@@ -3,8 +3,6 @@ from django.conf import settings
 from relecov_documentation import views
 from django.conf.urls.static import static
 
-from django.urls import include
-
 urlpatterns = [
     path("", views.index, name="index"),
     path(
@@ -17,22 +15,10 @@ urlpatterns = [
         views.initial_configuration,
         name="initial_configuration",
     ),
-    path("Installation/", views.installation, name="installation"),
-    path("Intranet/", views.intranet, name="intranet"),
-    path("Dashboard/", views.dashboard, name="dashboard"),
-    path("markdownx/", include("markdownx.urls")),
-    path("UploadMetadataLab/", views.upload_metadata_lab, name="upload_metadata_lab"),
-    path("ResultsDownload/", views.results_download, name="results_download"),
-    path(
-        "ResultsInfoProcessed/",
-        views.results_info_processed,
-        name="results_info_processed",
-    ),
-    path(
-        "ResultsInfoReceived/",
-        views.results_info_received,
-        name="results_info_received",
-    ),
+    path("installation/", views.installation, name="installation"),
+    path("intranet/", views.intranet, name="intranet"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("metadataLabForm/", views.upload_metadata_lab, name="upload_metadata_lab"),
     path("UploadToEna/", views.upload_to_ena, name="upload_to_ena"),
     path("UploadToGisaid/", views.upload_to_gisaid, name="upload_to_gisaid"),
     path("ApiUsage/", views.api_usage, name="api_usage"),
