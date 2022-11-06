f33aa776303171ae92f5c3caf4af7e429ecd2904
luissian <chapado.l@gmail.com>
2022-11-06 17:06:28 +0100

    Trying to get dash graphic lab for manager

---
+++ relecov_core/utils/plotly_dash_graphics.py
@@ -0,0 +1,95 @@
+from django_plotly_dash import DjangoDash
+from dash.dependencies import Input, Output
+from dash import dcc, html
+import plotly.express as px
+from plotly.offline import plot
+
+
+# from relecov_core.utils.handling_samples import get_sample_per_date_per_lab
+
+
+def dash_bar_lab(option_list, data):
+    def select_option(option, data):
+        """Return the sub data frame for the selected option"""
+        sub_df = data[data.lab_name == option]
+        # import pdb; pdb.set_trace()
+        return sub_df
+
+    option = []
+    for opt_list in option_list:
+        option.append({"label": opt_list, "value": opt_list})
+
+    app = DjangoDash("samplePerLabGraphic")
+    import pdb
+
+    """
+    if len(option) == 1:
+        graph = px.bar(
+            data, text_auto=True
+            # data, y=col_names[1], x=col_names[0], text_auto=True, width=options["width"]
+        )
+        # Customize aspect
+        graph.update_traces(
+            marker_color="rgb(158,202,225)",
+            marker_line_color="rgb(8,48,107)",
+            marker_line_width=1.5,
+            opacity=0.6,
+        )
+        graph.update_layout(
+            title=options["title"],
+            paper_bgcolor="rgba(0,0,0,0)",
+            plot_bgcolor="rgba(0,0,0,0)",
+            xaxis_tickangle=-45,
+            margin=dict(l=20, r=40, t=30, b=20),
+        )
+
+        plot_div = plot(graph, output_type="div", config={"displaylogo": False})
+
+    else:
+        import pdb; pdb.set_trace()
+    """
+    # pdb.set_trace()
+    if not data["lab_name"].is_unique:
+        graph = None
+    # choice = "Centro Militar de Veterinaria de la Defensa"
+    # sub_data = data[data.lab_name == choice]
+    # graph = px.bar(sub_data, x=sub_data["date"], y=sub_data["num_samples"], text_auto=True)
+    # pdb.set_trace()
+    app.layout = html.Div(
+        [
+            html.H4("Select the lab"),
+            html.Div(
+                [
+                    dcc.Dropdown(
+                        id="select_lab_name",
+                        options=option,
+                        clearable=False,
+                        multi=False,
+                        value=1,
+                        style={"width": "400px"},
+                    ),
+                ]
+            ),
+            html.Br(),
+            # dcc.Graph(id="bar_graph",figure=graph),
+            html.Div(id="my_selection"),
+        ]
+    )
+
+    @app.callback(Output("my_selection", "children"), Input("select_lab_name", "value"))
+    def update_graph(select_lab_name):
+        if select_lab_name == 1:
+            return None
+        sub_data = data[data.lab_name == select_lab_name]
+        return f"Laboratory selected: {select_lab_name}"
+        # pdb.set_trace()
+        # if select_lab_name == 1:
+        #    return None
+        sub_data = data[data.lab_name == select_lab_name]
+        # sub_data = select_option(select_lab_name, data)
+        graph = px.bar(
+            sub_data, x=sub_data["date"], y=sub_data["num_samples"], text_auto=True
+        )
+        # return f'Laboratory selected: {sub_data}'
+        #  pdb.set_trace()
+        return graph
