2eb69436e6dedc612d7a42693fdd6f5d153fcb23
luissian <chapado.l@gmail.com>
2022-11-10 23:14:51 +0100

    Created tables in admin and define Manager class objects for graphics

--- relecov_dashboard/models.py
+++ relecov_dashboard/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class GraphicNameManager(models.Manager):
+    def create_new_graphic_name(self, name):
+        return self.create(graphic_name=name)
+         
+
 class GraphicName(models.Model):
     graphic_name = models.CharField(max_length=80)
 
@@ -12,6 +17,18 @@ class GraphicName(models.Model):
 
     def get_graphic_name(self):
         return "%s" % (self.get_graphic_name)
+    
+    objects = GraphicNameManager()
+
+
+class GraphicFieldManager(models.Manager):
+    def create_new_graphic_field(self, data):
+        return self.create(
+            graphic=data["graphic_name_obj"],
+            field_1=data["field_1"],
+            field_2=data["field_2"],
+            field_3=data["field_3"],
+        )
 
 
 class GraphicField(models.Model):
@@ -26,6 +43,18 @@ class GraphicField(models.Model):
     def __str__(self):
         return "%s_%s" % (self.graphic, self.field_1)
 
+    objects = GraphicFieldManager()
+
+
+class GraphicValueManager(models.Manager):
+    def create_new_graphic_value(self, data):
+        return self.create(
+            graphic=data["graphic_name_obj"],
+            value_1=data["value_1"],
+            value_2=data["value_2"],
+            value_3=data["value_3"]
+        )
+
 
 class GraphicValue(models.Model):
     graphic = models.ForeignKey(GraphicName, on_delete=models.CASCADE)
@@ -38,3 +67,5 @@ class GraphicValue(models.Model):
 
     def __str__(self):
         return "%s_%s" % (self.graphic, self.value_1)
+
+    objects = GraphicValueManager()
