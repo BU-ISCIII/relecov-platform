116448c5edcd3934634ea62e04a2c2e8a0f57972
luissian <chapado.l@gmail.com>
2022-11-12 10:28:53 +0100

    liting

--- relecov_dashboard/utils/generic_functions.py
+++ relecov_dashboard/utils/generic_functions.py
@@ -9,11 +9,9 @@ def get_graphic_data(graphic_name):
         graphic_name_obj = GraphicName.objects.filter(
             graphic_name__iexact=graphic_name
         ).last()
-        fields = (
-            GraphicField.objects.filter(graphic=graphic_name_obj).values_list(
-                "field_1", "field_2", "field_3"
-            )[0]
-        )
+        fields = GraphicField.objects.filter(graphic=graphic_name_obj).values_list(
+            "field_1", "field_2", "field_3"
+        )[0]
         values = list(
             GraphicValue.objects.filter(graphic=graphic_name_obj).values_list(
                 "value_1", "value_2", "value_3"
