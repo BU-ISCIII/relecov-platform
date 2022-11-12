54ab0142f75391329f27dfb5e9a56bd00bc89ad8
luissian <chapado.l@gmail.com>
2022-11-12 09:57:45 +0100

    create file to pre processing the tables for graphics

--- relecov_dashboard/utils/pre_processing_data.py
+++ relecov_dashboard/utils/pre_processing_data.py
@@ -17,25 +17,27 @@ def pre_proc_lineages_variations():
     in_date_samples = fetch_samples_on_condition("collectionSampleDate")
     if "ERROR" in in_date_samples:
         return in_date_samples
-    all_lineages = list(
-        LineageValues.objects.filter(lineage_fieldID__property_name="lineage_name")
-        .values_list("value", flat=True)
-        .distinct()
-    )
-
     graphic_name_obj = GraphicName.objects.create_new_graphic_name(
         "lineages_variations"
     )
     field_names["graphic_name_obj"] = graphic_name_obj
     GraphicField.objects.create_new_graphic_field(field_names)
+
     for date, samples in in_date_samples["DATA"].items():
-        for lineage in all_lineages:
-            value_data = {"value_1": date, "value_2": lineage}
+        lineage_in_samples = (
+            LineageValues.objects.filter(
+                lineage_fieldID__property_name="lineage_name",
+                sample__collecting_lab_sample_id__in=samples,
+            )
+            .values_list("value", flat=True)
+            .distinct()
+        )
+        for lineage in lineage_in_samples:
+            value_data = {"value_1": date, "value_2": str(lineage)}
             value_data["value_3"] = Sample.objects.filter(
                 collecting_lab_sample_id__in=samples,
                 lineage_values__value__iexact=lineage,
             ).count()
             value_data["graphic_name_obj"] = graphic_name_obj
             GraphicValue.objects.create_new_graphic_value(value_data)
-
     return {"SUCCESS": "Success"}
