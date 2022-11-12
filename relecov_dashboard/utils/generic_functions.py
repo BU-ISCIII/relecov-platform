205c12298cdf7e65f8c2b8717a2da644918ae345
luissian <chapado.l@gmail.com>
2022-11-12 10:01:57 +0100

    changed to return a data frame

--- relecov_dashboard/utils/generic_functions.py
+++ relecov_dashboard/utils/generic_functions.py
@@ -1,22 +1,24 @@
+import pandas as pd
+
 from relecov_dashboard.models import GraphicName, GraphicField, GraphicValue
 
 
 def get_graphic_data(graphic_name):
     """Collect the pre-processed data from database"""
     if GraphicName.objects.filter(graphic_name__iexact=graphic_name).exists():
-        graphic_data = {}
         graphic_name_obj = GraphicName.objects.filter(
             graphic_name__iexact=graphic_name
         ).last()
-        graphic_data["fields"] = list(
+        fields = (
             GraphicField.objects.filter(graphic=graphic_name_obj).values_list(
-                "field_1", "field_2", "field_3", flat=True
-            )
+                "field_1", "field_2", "field_3"
+            )[0]
         )
-        graphic_data["values"] = list(
+        values = list(
             GraphicValue.objects.filter(graphic=graphic_name_obj).values_list(
-                "value_1", "value_2", "value_3", flat=True
+                "value_1", "value_2", "value_3"
             )
         )
-        return graphic_data
+        return pd.DataFrame(values, columns=fields)
+        
     return None
