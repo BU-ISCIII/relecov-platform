f140fb63e351b699e76b45b4203ef284ce8f03db
luissian <chapado.l@gmail.com>
2022-11-18 21:03:59 +0100

    Create empty pages for methodology according to menu

--- relecov_dashboard/urls.py
+++ relecov_dashboard/urls.py
@@ -9,9 +9,24 @@ urlpatterns = [
     # Methodology graphics
     path("methodology", views.methodology_index, name="methodology_index"),
     path(
-        "methodology/fields_utilization",
-        views.methodology_fields_utilization,
-        name="methodology_fields_utilization",
+        "methodology/hostInfo",
+        views.methodology_host_info,
+        name="methodology_host_info",
+    ),
+    path(
+        "methodology/sampleProcessing",
+        views.methodology_sample_processing,
+        name="methodology_sample_processing",
+    ),
+    path(
+        "methodology/sequencing",
+        views.methodology_sequencing,
+        name="methodology_sequencing",
+    ),
+    path(
+        "methodology/bioinfo",
+        views.methodology_bioinfo,
+        name="methodology_bioinfo",
     ),
     path("variants", views.variants_index, name="variants_index"),
     path(
@@ -80,7 +95,7 @@ urlpatterns = [
         name="spike_mutations_3D_BN",
     ),
     path("Gauge", views.gauge_test, name="gauge"),
-    
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
