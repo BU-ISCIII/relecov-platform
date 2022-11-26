39e1e07130f9b6fab9ccd9af3f9b5570b3b9071e
luissian <chapado.l@gmail.com>
2022-11-26 14:26:59 +0100

    modify the host age by using ranges

--- relecov_dashboard/dashboard_config.py
+++ relecov_dashboard/dashboard_config.py
@@ -1,1 +1,2 @@
 ERROR_NO_LINEAGES_ARE_DEFINED_YET = "There is no lineage defined yet"
+HOST_RANGE_AGE_TEXT = ["0 to 10", "11 to 20", "21 to 30", "31 to 40" ,"41 to 50", "51 to 60", "61 to 70", "71 to 80", "81 to 90" , "91 to 100" , "101 to 110", "111 to 120" ,"121 to 130"]
