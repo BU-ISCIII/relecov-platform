afec785ba1b971afbdf62b4a4d53b6450ec47391
luissian <chapado.l@gmail.com>
2022-12-07 23:55:46 +0100

    define new class to save processed data in json format

--- relecov_dashboard/models.py
+++ relecov_dashboard/models.py
@@ -69,3 +69,23 @@ class GraphicValue(models.Model):
         return "%s_%s" % (self.graphic, self.value_1)
 
     objects = GraphicValueManager()
+
+
+class GraphicJsonFileManager(models.Manager):
+    def create_new_graphic_json(self, data):
+        return self.create(
+            graphic_name=data["graphic_name"], graphic_data=data["graphic_data"]
+        )
+
+
+class GraphicJsonFile(models.Model):
+    graphic_name = models.CharField(max_length=60)
+    graphic_data = models.JSONField()
+
+    class Meta:
+        db_table = "GraphicJsonFile"
+
+    def __str__(self):
+        return "%s" % (self.graphic_name)
+
+    objects = GraphicJsonFileManager()
