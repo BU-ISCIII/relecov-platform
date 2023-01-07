50c7e48a565aa1a75bcc014826851d94afd12214
luissian <chapado.l@gmail.com>
2023-01-07 23:14:46 +0100

    Implemented time graphics for samples in the different handling steps

--- relecov_dashboard/utils/pre_processing_data.py
+++ relecov_dashboard/utils/pre_processing_data.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from relecov_dashboard.models import GraphicJsonFile
 from relecov_core.utils.handling_variant import (
     get_domains_and_coordenates,
@@ -12,12 +13,108 @@ from relecov_core.models import (
     Sample,
     VariantInSample,
     VariantAnnotation,
+    BioinfoAnalysisValue,
 )
 
 from relecov_core.utils.rest_api_handling import (
     fetch_samples_on_condition,
     get_stats_data,
+    get_sample_parameter_data,
 )
+
+
+def pre_proc_calculation_date():
+    """Fetch the information about date for each sample to know about the
+    number of days between different steps of samples
+    """
+
+    def convert_data_to_sample_dict(data, data_1, data_2):
+        out_data = {}
+        for item in data:
+            if not item[data_1] in out_data:
+                out_data[item[data_1]] = item[data_2]
+        return out_data
+
+    def convert_str_to_datetime(data, separator):
+        """Convert the string values to datetime object to perform calculation
+        dates
+        """
+        if separator:
+            d_format = "%Y" + separator + "%m" + separator + "%d"
+        else:
+            d_format = "%Y%m%d"
+        for sample in data.keys():
+            if data[sample]:
+                data[sample] = datetime.strptime(data[sample], d_format)
+        return data
+
+    def calculate_days(sample_list, date_1, date_2):
+        """Function gets 2 dictionnary list. For the same sample the substract
+        operation date_2 - date_1 is done.
+        """
+        out_data = []
+        for sample in sample_list:
+            if sample in date_1 and sample in date_2:
+                try:
+                    days = (date_2[sample] - date_1[sample]).days
+                    if (date_2[sample] - date_1[sample]).days > 1000:
+                        print(sample, "numero dias ", days)
+                    out_data.append((date_2[sample] - date_1[sample]).days)
+                except TypeError:
+                    out_data.append("bbbb")
+            else:
+                out_data.append("aaaaaa")
+        return out_data
+
+    # get sequencing date from sample table
+    analysis_date = BioinfoAnalysisValue.objects.filter(
+        bioinfo_analysis_fieldID__property_name__exact="analysis_date",
+    ).values("value", "sample__collecting_lab_sample_id")
+    analysis_date = convert_data_to_sample_dict(
+        analysis_date, "sample__collecting_lab_sample_id", "value"
+    )
+    analysis_date = convert_str_to_datetime(analysis_date, None)
+
+    seq_date = Sample.objects.all().values(
+        "collecting_lab_sample_id", "sequencing_date"
+    )
+    seq_date = convert_data_to_sample_dict(
+        seq_date, "collecting_lab_sample_id", "sequencing_date"
+    )
+
+    # send request to iSkyLIMS
+    collection_date = get_sample_parameter_data("collectionSampleDate")
+    collection_date = convert_data_to_sample_dict(
+        collection_date, "Sample Name", "collectionSampleDate"
+    )
+    collection_date = convert_str_to_datetime(collection_date, "-")
+
+    recorded_date = get_sample_parameter_data("sampleEntryDate")
+    recorded_date = convert_data_to_sample_dict(
+        recorded_date, "Sample Name", "sampleEntryDate"
+    )
+    recorded_date = convert_str_to_datetime(recorded_date, "-")
+
+    # perform calculation dates
+    calculation_dates = {}
+    sample_list = list(seq_date.keys())
+    # calculation_dates["samples"] = sample_list
+    calculation_dates["coll_rec_date"] = calculate_days(
+        sample_list, collection_date, recorded_date
+    )
+    calculation_dates["seq_coll_date"] = calculate_days(
+        sample_list, recorded_date, seq_date
+    )
+    calculation_dates["analyis_seq_date"] = calculate_days(
+        sample_list, seq_date, analysis_date
+    )
+    # json_data = json.dumps(calculation_dates)
+    # Save json in database
+    GraphicJsonFile.objects.create_new_graphic_json(
+        {"graphic_name": "calculation_date", "graphic_data": calculation_dates}
+    )
+
+    return {"SUCCESS": "Success"}
 
 
 def pre_proc_lineages_variations():
