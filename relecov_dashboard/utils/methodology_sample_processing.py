614f8187ab8309ad04481b65da712a470d274353
luissian <chapado.l@gmail.com>
2023-01-07 23:14:46 +0100

    Implemented time graphics for samples in the different handling steps

--- relecov_dashboard/utils/methodology_sample_processing.py
+++ relecov_dashboard/utils/methodology_sample_processing.py
@@ -2,6 +2,7 @@ import pandas as pd
 from relecov_dashboard.utils.pre_processing_data import (
     pre_proc_specimen_source_pcr_1,
     pre_proc_extraction_protocol_pcr_1,
+    pre_proc_calculation_date,
 )
 from relecov_core.utils.rest_api_handling import get_stats_data
 from relecov_dashboard.utils.generic_functions import get_graphic_json_data
@@ -14,23 +15,28 @@ def sample_processing_graphics():
         If there is not data stored for the graphic, it will query to store
         them before calling for the second time
         """
-        map_graphic = {"extraction_protocol_pcr_1" : "pre_proc_specimen_source_pcr_1", "specimen_source_pcr_1": "pre_proc_specimen_source_pcr_1", "calculation_date": "pre_proc_calculation_date"}
+
+        # map_graphic = {"extraction_protocol_pcr_1" : pre_proc_specimen_source_pcr_1(), "specimen_source_pcr_1": pre_proc_extraction_protocol_pcr_1(), "calculation_date": pre_proc_calculation_date()}
 
         json_data = get_graphic_json_data(graphic_name)
         if json_data is None:
             # Execute the pre-processed task to get the data
-            result = eval(map_graphic[graphic_name] + "()")
-            """
-            result = pre_proc_specimen_source_pcr_1()
+            if graphic_name == "extraction_protocol_pcr_1":
+                result = pre_proc_specimen_source_pcr_1()
+            elif graphic_name == "specimen_source_pcr_1":
+                result = pre_proc_extraction_protocol_pcr_1()
+            elif graphic_name == "calculation_date":
+                result = pre_proc_calculation_date()
+            else:
+                return {"ERROR": "pre-processing not defined"}
             if "ERROR" in result:
                 return result
-            result = pre_proc_extraction_protocol_pcr_1()
-            """
-            if "ERROR" in result:
-                return result
-        json_data = get_graphic_json_data(graphic_name)
+            json_data = get_graphic_json_data(graphic_name)
         # Convert string to float values
+        if graphic_name == "calculation_date":
+            return [json_data]
         data = []
+
         for key, values in json_data.items():
             tmp_data = []
             for str_val, numbers in values.items():
@@ -71,6 +77,7 @@ def sample_processing_graphics():
             return pd.DataFrame(lims_data.items(), columns=columns)
 
     sample_processing = {}
+
     # extraction protocol graphics
     extraction_protocol_df = fetching_data_for_sample_processing(
         project_field="nucleic_acid_extraction_protocol", columns=["protocol", "number"]
@@ -88,19 +95,24 @@ def sample_processing_graphics():
             "width": 320,
         },
     )
+
     cts_extraction_data = get_pre_proc_data("extraction_protocol_pcr_1")
 
     sample_processing["cts_extraction"] = box_plot_graphic(
         cts_extraction_data,
-        {"title": "Boxplot Cts / Extraction protocol", "height": 400, "width": 420},
+        {"title": "Boxplot Cts / Extraction protocol", "height": 400, "width": 520},
     )
     # expecimen source graphics
     cts_specimen_data = get_pre_proc_data("specimen_source_pcr_1")
 
     sample_processing["cts_specimen"] = box_plot_graphic(
         cts_specimen_data,
-        {"title": "Boxplot Cts / specimen source", "height": 400, "width": 520},
+        {"title": "Boxplot Cts / specimen source", "height": 400, "width": 670},
     )
     # calculate the number of days spent in each state before moved on to next step
-    calculation_date = get_pre_proc_data("calculation_date")
+    calculation_date_data = get_pre_proc_data("calculation_date")
+    sample_processing["calculation_date"] = box_plot_graphic(
+        calculation_date_data,
+        {"title": "Time between sample step actions", "height": 400, "width": 420},
+    )
     return sample_processing
