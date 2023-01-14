abe5325994194970804981c7f8cbf7e27808175f
luissian <chapado.l@gmail.com>
2023-01-14 14:08:27 +0100

    Created graphic for CTs and number of base pairs sequenced

--- relecov_dashboard/utils/methodology_sequencing.py
+++ relecov_dashboard/utils/methodology_sequencing.py
@@ -1,12 +1,20 @@
 import pandas as pd
-from relecov_dashboard.utils.plotly_graphics import bar_graphic, box_plot_graphic
+from statistics import mean
+from relecov_dashboard.utils.plotly_graphics import (
+    bar_graphic,
+    box_plot_graphic,
+    line_graphic,
+)
 from relecov_core.utils.rest_api_handling import get_stats_data
 from relecov_dashboard.utils.generic_functions import get_graphic_json_data
-from relecov_dashboard.utils.pre_processing_data import pre_proc_library_kit_pcr_1
+from relecov_dashboard.utils.pre_processing_data import (
+    pre_proc_library_kit_pcr_1,
+    pre_proc_based_pairs_sequenced,
+)
 
 
 def sequencing_graphics():
-    def get_pre_proc_data(graphic_name):
+    def get_pre_proc_data(graphic_name, out_format):
         """Get the pre-processed data for the graphic name.
         If there is not data stored for the graphic, it will query to store
         them before calling for the second time
@@ -14,22 +22,32 @@ def sequencing_graphics():
         json_data = get_graphic_json_data(graphic_name)
         if json_data is None:
             # Execute the pre-processed task to get the data
-
-            result = pre_proc_library_kit_pcr_1()
+            if graphic_name == "library_kit_pcr_1":
+                result = pre_proc_library_kit_pcr_1()
+            elif graphic_name == "ct_number_of_base_pairs_sequenced":
+                result = pre_proc_based_pairs_sequenced()
+            else:
+                return {"ERROR": "pre-processing not defined"}
             if "ERROR" in result:
                 return result
         json_data = get_graphic_json_data(graphic_name)
-        # Convert string to float values
-        data = []
-        for key, values in json_data.items():
-            tmp_data = []
-            for str_val, numbers in values.items():
-                try:
-                    float_val = float(str_val)
-                except ValueError:
-                    continue
-                tmp_data += [float_val] * numbers
-            data.append({key: tmp_data})
+        if out_format == "list_of_dict":
+            data = []
+            for key, values in json_data.items():
+                # Convert string to float values
+                tmp_data = []
+                for str_val, numbers in values.items():
+                    try:
+                        float_val = float(str_val)
+                    except ValueError:
+                        continue
+                    tmp_data += [float_val] * numbers
+                data.append({key: tmp_data})
+        else:
+            data = {"based": [], "cts": []}
+            for key, values in json_data.items():
+                data["based"].append(int(key))
+                data["cts"].append(mean(values))
         return data
 
     def fetching_data_for_sequencing_data(project_field, columns):
@@ -94,9 +112,22 @@ def sequencing_graphics():
     )
     # box plot for library preparation kit
 
-    cts_library_data = get_pre_proc_data("library_kit_pcr_1")
+    cts_library_data = get_pre_proc_data("library_kit_pcr_1", "list_of_dict")
     sequencing["cts_library"] = box_plot_graphic(
         cts_library_data,
         {"title": "Boxplot Cts / Library preparation kit", "height": 400, "width": 420},
+    )
+
+    cts_pcr_1 = get_pre_proc_data("ct_number_of_base_pairs_sequenced", "dict")
+    sequencing["number_of_base"] = line_graphic(
+        cts_pcr_1["based"],
+        cts_pcr_1["cts"],
+        {
+            "title": "Number of base",
+            "height": 350,
+            "width": 300,
+            "x_title": "Number of base pairs",
+            "y_title": "PCR CT 1",
+        },
     )
     return sequencing
