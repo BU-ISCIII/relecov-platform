233460856accffe0f4f82034d1ff923e3065c02e
luissian <chapado.l@gmail.com>
2023-01-15 12:13:19 +0100

    Added figure in bioinfo methodology depth--variants

--- relecov_dashboard/utils/methodology_sequencing.py
+++ relecov_dashboard/utils/methodology_sequencing.py
@@ -30,7 +30,7 @@ def sequencing_graphics():
                 return {"ERROR": "pre-processing not defined"}
             if "ERROR" in result:
                 return result
-        json_data = get_graphic_json_data(graphic_name)
+            json_data = get_graphic_json_data(graphic_name)
         if out_format == "list_of_dict":
             data = []
             for key, values in json_data.items():
