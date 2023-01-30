d53e4ab0c19f2a155cfb7274c2f3a3210b28ca11
Sara Monzón <sara.monzon.fdz@gmail.com>
2023-01-30 16:48:16 +0100

    added sorted static folders

---
+++ relecov_core/static/relecov_core/custom/js/index_functions.js
@@ -0,0 +1,22 @@
+function setCounters(){
+    const counters = document.querySelectorAll('.value');
+            const speed = 500;
+
+            counters.forEach( counter => {
+            const animate = () => {
+                const value = +counter.getAttribute('akhi');
+                const data = +counter.innerText;
+
+                const time = value / speed;
+                if(data < value) {
+                    counter.innerText = Math.ceil(data + time);
+                    setTimeout(animate, 1);
+                    }else{
+                    counter.innerText = value;
+                    }
+                }
+            animate();
+            });
+  }
+
+ setCounters();
