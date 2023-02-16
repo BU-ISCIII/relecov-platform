644482485b847253d04c48b93bce885f04fb9260
Sara Monzón <sara.monzon.fdz@gmail.com>
2023-02-16 12:16:42 +0100

    update urls and views to docs index

--- relecov_documentation/urls.py
+++ relecov_documentation/urls.py
@@ -6,20 +6,31 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", views.index, name="index"),
     path(
-        "createNewUserAccount/",
-        views.create_new_user_account,
-        name="create_new_user_account",
+        "relecovInstall/", views.relecov_install, name="relecov_install"
     ),
-    path("configuration", views.configuration, name="configuration"),
-    path(
-        "relecovInstallation/", views.relecov_installation, name="relecov_installation"
-    ),
+    path("configuration/", views.configuration, name="configuration"),
+
+    path("metadata/", views.metadata, name="metadata"),
+    path("metadataLabExcel/", views.metadata_lab_excel, name="metadata_lab_excel"),
+    path("relecovTools/", views.relecov_tools, name="relecov_tools"),
     path("intranet/", views.intranet, name="intranet"),
-    path("dashboard/", views.dashboard, name="dashboard"),
-    path("metadataLabForm/", views.upload_metadata_lab, name="upload_metadata_lab"),
+    path("intranetDashboard/", views.intranet_dashboard, name="intranet_dashboard"),
+    path(
+        "uploadMetadataLab/", views.upload_metadata_lab, name="upload_metadata_lab"
+        ),
+    path("variantDashboard/", views.variant_dashboard, name="variant_dashboard"),
+    path("methodologyDashboard/", views.methodology_dashboard, name="methodology_dashboard"),
+    path("nextstrainInstall/", views.nextstrain_install, name="nextstrain_install"),
+    path("howtoNextstrain/", views.howto_nextstrain, name="howto_nextstrain"),
     path("UploadToEna/", views.upload_to_ena, name="upload_to_ena"),
     path("UploadToGisaid/", views.upload_to_gisaid, name="upload_to_gisaid"),
-    path("ApiUsage/", views.api_usage, name="api_usage"),
+    path("ApiSchema/", views.api_schema, name="api_schema"),
+    path("HowtoApi/", views.howto_api, name="howto_api"),
+    path(
+        "createNewUser/",
+        views.create_new_user,
+        name="create_new_user",
+    ),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
