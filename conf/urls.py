738956906b1eae3273036d15449e286c81def09d
luissian <chapado.l@gmail.com>
2023-03-01 22:47:35 +0100

    included drf spectacular for API documentation

--- conf/urls.py
+++ conf/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from drf_yasg.generators import OpenAPISchemaGenerator
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
+# from drf_yasg.generators import OpenAPISchemaGenerator
+from drf_spectacular.views import SpectacularRedocView, SpectacularSwaggerView, SpectacularAPIView
 
-
+"""
 class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
     def get_schema(self, request=None, public=False):
         schema = super().get_schema(request, public)
@@ -21,16 +22,19 @@ schema_view = get_schema_view(
     generator_class=BothHttpAndHttpsSchemaGenerator,
     public=True,
 )
-
+"""
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("relecov_core.urls")),
     path("dashboard/", include("relecov_dashboard.urls")),
     path("documentation/", include("relecov_documentation.urls")),
+    # API REST FULL using drf spectacular
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("swagger/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     # REST FRAMEWORK URLS
     path("api/", include("relecov_core.api.urls")),
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0)),
     # user accounts
     path("accounts/", include("django.contrib.auth.urls")),
     # path('markdownx/', include('markdownx.urls')),
