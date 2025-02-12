9211d4c31bbf4368f3c3fed87eb3d3466e2bc741
OPSergio <solmos97@gmail.com>
2025-02-12 15:47:00 +0100

    Added browse-data to urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -51,6 +51,11 @@ urlpatterns = [
         dashboard.views.lineages_voc,
         name="lineages_voc",
     ),
+    path(
+        "browse-data/", 
+        dashboard.views.browse_data,
+        name= 'browse_data'
+    )
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
