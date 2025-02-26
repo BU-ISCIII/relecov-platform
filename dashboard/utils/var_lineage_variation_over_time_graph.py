addd8118eff2fb0584592bbe4880ea4e2cbd461e
Shettland <pyblus@gmail.com>
2025-02-26 16:56:39 +0100

    Now lineages VOC is shown based on Collection date as ISOWeeks

--- dashboard/utils/var_lineage_variation_over_time_graph.py
+++ dashboard/utils/var_lineage_variation_over_time_graph.py
@@ -12,7 +12,6 @@ from plotly.subplots import make_subplots
 # Local imports
 import dashboard.utils.generic_graphic_data
 import dashboard.utils.generic_process_data
-import core.utils.generic_functions
 
 
 def create_lineages_variations_graphic():
@@ -97,6 +96,7 @@ def create_lineages_variations_graphic():
                 (data_df["Collection date"] >= first_date)
                 & (data_df["Collection date"] < last_date)
             ]
+
         else:
             start_date_obj = datetime.strptime(start_date, "%Y-%m-%d")
             end_date_obj = datetime.strptime(end_date, "%Y-%m-%d")
@@ -104,11 +104,9 @@ def create_lineages_variations_graphic():
                 (data_df["Collection date"] >= start_date_obj)
                 & (data_df["Collection date"] < end_date_obj)
             ]
-
+        sub_data_df = sub_data_df.sort_values("Collection date") # Order by date to ensure isoweeks are ordered too
         samples_df = pd.DataFrame()
-
         samples_df["samples"] = sub_data_df.groupby("Collection date")["samples"].sum()
-        # reset_index
         samples_df = samples_df.reset_index()
         # samples_df["samples_moving_mean"] = samples_df["samples"].rolling(7).mean()
 
@@ -125,7 +123,8 @@ def create_lineages_variations_graphic():
             .reset_index()
             .sort_values("Collection date")
         )
-        graph_df = data_week_df.set_index(["Lineage", "Collection date"]).unstack(
+        data_week_df["Collection ISOWeek"] = data_week_df["Collection date"].dt.strftime("%Y-W%V")
+        graph_df = data_week_df.drop("Collection date", axis=1).set_index(["Lineage", "Collection ISOWeek"]).unstack(
             ["Lineage"]
         )
 
@@ -139,7 +138,33 @@ def create_lineages_variations_graphic():
         # value_per_df = value_per_df
         # Create figure with secondary y-axis
         fig = make_subplots(specs=[[{"secondary_y": True}]])
-
+        if sub_data_df.empty:
+            fig.add_trace(go.Scatter())
+            fig.add_annotation(
+                text="No samples found for the selected dates",
+                xref="paper", yref="paper",
+                x=0.5, y=0.5, showarrow=False,
+                font=dict(size=20, color="red")
+            )
+            fig.update_layout(
+                xaxis=dict(visible=False),
+                yaxis=dict(visible=False),
+                barmode="stack",
+                hovermode="x unified",
+                legend_xanchor="center",  # use center of legend as anchor
+                legend_yanchor="top",
+                legend_orientation="h",  # show entries horizontally
+                legend_x=0.5,  # put legend in center of x-axis
+                legend_y=-0.25,
+                bargap=0,  # gap between bars of adjacent location coordinates.
+                bargroupgap=0,  # gap between bars of the same location coordinate.
+                margin_l=10,
+                margin_r=10,
+                margin_b=40,
+                margin_t=30,
+                height=600,
+            )
+            return fig
         fig.add_trace(
             go.Scatter(
                 x=value_per_df.index,
@@ -164,12 +189,10 @@ def create_lineages_variations_graphic():
                 ),
                 secondary_y=False,
             )
-
         # Set x-axis title
         fig.update_xaxes(
-            title_text="Collection Date",
+            title='Collection Date (ISOweeks)',
         )
-
         # Set y-axes titles
         fig.update_yaxes(
             range=[0, 100], title_text="<b>Lineage % relative", secondary_y=False
@@ -177,15 +200,17 @@ def create_lineages_variations_graphic():
         fig.update_yaxes(
             title_text="<b>Number of samples processed</b>", secondary_y=True
         )
-
         # Add figure title
         fig.update_layout(
             title_text="Variants over the selected period",
+            autotypenumbers="convert types",
             barmode="stack",
             hovermode="x unified",
             legend_xanchor="center",  # use center of legend as anchor
+            legend_yanchor="top",
             legend_orientation="h",  # show entries horizontally
             legend_x=0.5,  # put legend in center of x-axis
+            legend_y=-0.25,
             bargap=0,  # gap between bars of adjacent location coordinates.
             bargroupgap=0,  # gap between bars of the same location coordinate.
             margin_l=10,
