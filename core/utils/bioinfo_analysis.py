cf9afb060c5ada375c09d3bfb34a74b18a239299
Shettland <pyblus@gmail.com>
2025-02-27 08:42:45 +0100

    Small query improvements. Linting

--- core/utils/bioinfo_analysis.py
+++ core/utils/bioinfo_analysis.py
@@ -20,10 +20,16 @@ def get_bio_analysis_stats_from_lab(lab_name=None):
         lab_samples = core.models.Sample.objects.filter(
             collecting_institution__iexact=lab_name
         )
-        bio_stats["analized"] = lab_samples.select_related("state_id").filter(
-            state_id__state__iexact="Bioinfo"
-        ).values("sequencing_sample_id").distinct().count()
-        bio_stats["received"] = lab_samples.values("sequencing_sample_id").distinct().count()
+        bio_stats["analized"] = (
+            lab_samples.select_related("state_id")
+            .filter(state_id__state__iexact="Bioinfo")
+            .values("sequencing_sample_id")
+            .distinct()
+            .count()
+        )
+        bio_stats["received"] = (
+            lab_samples.values("sequencing_sample_id").distinct().count()
+        )
     return bio_stats
 
 
