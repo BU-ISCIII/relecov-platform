e71a3e8fd880b6325ea640223ec686e9b0a45d6b
Shettland <pyblus@gmail.com>
2025-02-27 08:41:27 +0100

    Included pre-proc-samples-per-date-all-lab() in cron. Linting

--- dashboard/cron.py
+++ dashboard/cron.py
@@ -84,7 +84,9 @@ def update_graphic_json_data():
     print("Running pre_proc_samples_per_date_all_lab()")
     dashboard.utils.generic_process_data.pre_proc_samples_per_date_all_lab()
     print("Running pre_proc_samples_per_date_all_lab(detailed=True)")
-    dashboard.utils.generic_process_data.pre_proc_samples_per_date_all_lab(detailed=True)
+    dashboard.utils.generic_process_data.pre_proc_samples_per_date_all_lab(
+        detailed=True
+    )
     print(f"List of extracted unique chromosomes: {uniq_chrom_id_list}")
     print("Running pre_proc_variations_per_lineage() for each chromosome")
     for chromosome in uniq_chrom_id_list:
