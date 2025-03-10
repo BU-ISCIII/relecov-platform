bd9a3d7505262b1e61edcc376c135f5c5afa53ba
Shettland <pyblus@gmail.com>
2025-03-10 17:10:43 +0100

    Getting ena and gisaid data from pre-processed state

--- core/utils/public_db.py
+++ core/utils/public_db.py
@@ -56,31 +56,43 @@ def get_public_information_from_sample(p_type, sample_id):
 
 def get_preprocessed_gisaid_data():
     """Load preprocessed gisaid data in graphic_json table"""
-    gisaid_data = dashboard.utils.generic_graphic_data.get_graphic_json_data(
-        "intranet_gisaid_data"
+    gisaid_data = (
+        dashboard.utils.generic_graphic_data.get_graphic_json_data(
+            "intranet_gisaid_data"
+        )
     )
     if gisaid_data is None:
         # Execute the pre-processed task to get the data
-        result = dashboard.utils.generic_process_data.pre_proc_intranet_gisaid_data()
+        result = (
+            dashboard.utils.generic_process_data.pre_proc_intranet_gisaid_data()
+        )
         if "ERROR" in result:
             return result
-        gisaid_data = dashboard.utils.generic_graphic_data.get_graphic_json_data(
-            "intranet_gisaid_data"
+        gisaid_data = (
+            dashboard.utils.generic_graphic_data.get_graphic_json_data(
+                "intranet_gisaid_data"
+            )
         )
     return gisaid_data
 
 
 def get_preprocessed_ena_data():
     """Load preprocessed ena data in graphic_json table"""
-    ena_data = dashboard.utils.generic_graphic_data.get_graphic_json_data(
-        "intranet_ena_data"
+    ena_data = (
+        dashboard.utils.generic_graphic_data.get_graphic_json_data(
+            "intranet_ena_data"
+        )
     )
     if ena_data is None:
         # Execute the pre-processed task to get the data
-        result = dashboard.utils.generic_process_data.pre_proc_intranet_ena_data()
+        result = (
+            dashboard.utils.generic_process_data.pre_proc_intranet_ena_data()
+        )
         if "ERROR" in result:
             return result
-        ena_data = dashboard.utils.generic_graphic_data.get_graphic_json_data(
-            "intranet_ena_data"
+        ena_data = (
+            dashboard.utils.generic_graphic_data.get_graphic_json_data(
+                "intranet_ena_data"
+            )
         )
     return ena_data
