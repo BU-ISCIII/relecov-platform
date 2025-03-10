cc901929bfa09ad4aa4540ccce2c16832b3e3992
Shettland <pyblus@gmail.com>
2025-03-10 17:15:12 +0100

    Get samples per lab and ccaa from pre-processed data

--- core/utils/samples_graphics.py
+++ core/utils/samples_graphics.py
@@ -4,12 +4,13 @@ import core.utils.rest_api
 import core.utils.samples
 import dashboard.utils.generic_process_data
 
-
 def received_per_ccaa():
     """Fetch the data from LIMS and show them in a graphic bar"""
-    samples_per_ccaa = dashboard.utils.generic_graphic_data.get_graphic_json_data(
-        "samples_received_per_ccaa"
-    )
+    samples_per_ccaa = (
+            dashboard.utils.generic_graphic_data.get_graphic_json_data(
+                "samples_received_per_ccaa"
+            )
+        )
     if samples_per_ccaa is None:
         # Execute the pre-processed task to get the data
         result = (
@@ -17,8 +18,10 @@ def received_per_ccaa():
         )
         if "ERROR" in result:
             return result
-        samples_per_ccaa = dashboard.utils.generic_graphic_data.get_graphic_json_data(
-            "samples_received_per_ccaa"
+        samples_per_ccaa = (
+            dashboard.utils.generic_graphic_data.get_graphic_json_data(
+                "samples_received_per_ccaa"
+            )
         )
     return core.utils.plotly_graphics.bar_graphic(
         data=samples_per_ccaa,
@@ -31,9 +34,11 @@ def received_per_ccaa():
 
 def received_per_lab():
     """Fetch the data from LIMS and show them in a graphic bar"""
-    samples_per_lab = dashboard.utils.generic_graphic_data.get_graphic_json_data(
-        "samples_received_per_lab"
-    )
+    samples_per_lab = (
+            dashboard.utils.generic_graphic_data.get_graphic_json_data(
+                "samples_received_per_lab"
+            )
+        )
     if samples_per_lab is None:
         # Execute the pre-processed task to get the data
         result = (
@@ -41,8 +46,10 @@ def received_per_lab():
         )
         if "ERROR" in result:
             return result
-        samples_per_lab = dashboard.utils.generic_graphic_data.get_graphic_json_data(
-            "samples_received_per_lab"
+        samples_per_lab = (
+            dashboard.utils.generic_graphic_data.get_graphic_json_data(
+                "samples_received_per_lab"
+            )
         )
     return core.utils.plotly_graphics.bar_graphic(
         data=samples_per_lab,
