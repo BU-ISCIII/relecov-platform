e1652134a0b9c10d40896c3b2205bf8ca4e4580b
OPSergio <solmos97@gmail.com>
2025-04-08 11:21:23 +0200

    Removed old infographic

--- dashboard/utils/plotly.py
+++ dashboard/utils/plotly.py
@@ -245,7 +245,7 @@ def pie_graphic(labels, values, options, show_legend=True):
         hoverinfo="label+percent",
         textinfo="value",
         textfont_size=16,
-        template=mi_template,
+        title_font=dict(size=18, family="Verdana", color="#448873"),
         marker=dict(colors=colors),
         opacity=0.6,
     )
