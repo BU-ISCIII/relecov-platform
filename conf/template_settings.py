50fd7f4246fea6cc5735b5404024d443850e246e
OPSergio <solmos97@gmail.com>
2025-04-16 15:49:40 +0200

    Renamed context processor to avoid possible key name collision (docs_current_path instead of current_path)

--- conf/template_settings.py
+++ conf/template_settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "docs.utils.context_processors.current_path",
+                "docs.utils.context_processors.docs_current_path",
             ],
         },
     },
