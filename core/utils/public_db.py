a58cdd4982b87c26751e94b768d11dc78680093f
Shettland <pyblus@gmail.com>
2025-05-16 14:48:33 +0200

    Introduced config FIELD_EMPTY_VALUES to gather all values that mean not-provided.

--- core/utils/public_db.py
+++ core/utils/public_db.py
@@ -2,6 +2,7 @@
 import core.models
 import core.utils.plotly_graphics
 import dashboard.utils.generic_graphic_data
+import core.config
 from django.db.models import Q
 
 
@@ -14,7 +15,7 @@ def get_public_accession_from_sample_lab(p_field, sample_objs=None):
             core.models.PublicDatabaseValues.objects.filter(
                 public_database_fieldID__property_name__iexact=p_field,
             )
-            .exclude(Q(value__icontains="Not Provided") | Q(value__isnull=True))
+            .exclude(Q(value__in=core.config.FIELD_EMPTY_VAUES) | Q(value__isnull=True))
             .values_list(
                 "sampleID__submitting_institution",
                 "sampleID__sequencing_sample_id",
@@ -27,7 +28,7 @@ def get_public_accession_from_sample_lab(p_field, sample_objs=None):
                 sampleID__in=sample_objs,
                 public_database_fieldID__property_name__exact=p_field,
             )
-            .exclude(Q(value__icontains="Not Provided") | Q(value__isnull=True))
+            .exclude(Q(value__in=core.config.FIELD_EMPTY_VAUES) | Q(value__isnull=True))
             .values_list("sampleID__sequencing_sample_id", "value")
         )
 
