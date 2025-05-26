145129ddbf323d2e3eb33b0d216d9afa9661e222
OPSergio <solmos97@gmail.com>
2025-05-26 17:22:01 +0200

    Fix unstack error in Lineages VOC dashboard by aggregating samples per week and lineage before pivoting

--- dashboard/utils/var_lineage_variation_over_time_graph.py
+++ dashboard/utils/var_lineage_variation_over_time_graph.py
@@ -147,11 +147,12 @@ def create_lineages_variations_graphic():
         sub_data_df["Collection ISOWeek"] = sub_data_df["Collection date"].dt.strftime(
             "%Y-W%V"
         )
+        
+        agg_df = sub_data_df.groupby(["Lineage", "Collection ISOWeek"])["samples"].sum().reset_index()
 
         graph_df = (
-            sub_data_df.drop(["Collection date"], axis=1)
-            .set_index(["Lineage", "Collection ISOWeek"])
-            .unstack(["Lineage"])
+            agg_df.set_index(["Collection ISOWeek", "Lineage"])
+            .unstack("Lineage")
         )
 
         # remove the sample text from column
