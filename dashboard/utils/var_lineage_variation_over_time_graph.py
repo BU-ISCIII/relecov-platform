dafa45819ce2b8265495250bf10f67000e148a0a
OPSergio <solmos97@gmail.com>
2025-05-26 18:21:20 +0200

    Fixed format

--- dashboard/utils/var_lineage_variation_over_time_graph.py
+++ dashboard/utils/var_lineage_variation_over_time_graph.py
@@ -147,12 +147,11 @@ def create_lineages_variations_graphic():
         sub_data_df["Collection ISOWeek"] = sub_data_df["Collection date"].dt.strftime(
             "%Y-W%V"
         )
-        
+
         agg_df = sub_data_df.drop_duplicates(subset=["Lineage", "Collection ISOWeek"])
-        
-        graph_df = (
-            agg_df.set_index(["Collection ISOWeek", "Lineage"])
-            .unstack("Lineage")
+
+        graph_df = agg_df.set_index(["Collection ISOWeek", "Lineage"]).unstack(
+            "Lineage"
         )
 
         # remove the sample text from column
