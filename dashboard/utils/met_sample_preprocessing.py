fcdee35e5ff39106bcd9c1660609b044adbb3c19
Alejandro <aberdur7@gmail.com>
2025-06-06 11:53:55 +0200

    Add Ct maximum filter of 40 to never collapse graph

--- dashboard/utils/met_sample_preprocessing.py
+++ dashboard/utils/met_sample_preprocessing.py
@@ -50,6 +50,8 @@ def sample_processing_graphics():
                     float_val = float(str_val)
                 except ValueError:
                     continue
+                if float_val > 40:
+                    continue
                 tmp_data += [float_val] * numbers
             data.append({key: tmp_data})
         return data
