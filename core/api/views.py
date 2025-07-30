f14cf7b2fa7ffdf5948e8a31f948be9f733c686c
Sara Monzón <sara.monzon.fdz@gmail.com>
2025-07-30 16:26:16 +0200

    fix check_sample_exists response so it's a json not an object

--- core/api/views.py
+++ core/api/views.py
@@ -1053,10 +1053,8 @@ def check_sample_exists(request):
     )
     found_sample = core.utils.samples.get_sample_obj_from_fingerprint(temp_fingerprint)
     if found_sample:
-        return Response(
-            {"message": "sample correctly found", "data": found_sample},
-            status=status.HTTP_200_OK,
-        )
+        sample_serialized = core.api.serializers.CreateSampleSerializer(found_sample)
+        return Response({"detail": sample_serialized.data}, status=status.HTTP_200_OK)
     else:
         return Response(
             {"message": "Sample not found.", "data": {}}, status=status.HTTP_200_OK
