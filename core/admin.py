1c476331fae75e456bc0b05887f2acd6e4b50b89
Shettland <pyblus@gmail.com>
2025-08-28 12:00:03 +0200

    Included a new form to modify Schemas in-place from admin. fix

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from django import forms
-from django.core.exceptions import ValidationError
 import json
 import os
 
