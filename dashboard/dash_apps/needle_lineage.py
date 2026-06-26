28ab38e78e15bad87d17a5090c177b2105310c23
svarona <sarai.var.fer@gmail.com>
2026-06-26 09:34:42 +0200

    Added font to plotly and other HTML files

--- dashboard/dash_apps/needle_lineage.py
+++ dashboard/dash_apps/needle_lineage.py
@@ -80,7 +80,8 @@ def register():
                     ),
                 ],
             ),
-        ]
+        ],
+        style={"fontFamily": "Oxanium, sans-serif"},
     )
 
     @app.callback(
